251bb30a7c8c0fc084fc0fcbaa192867538452b1
Dan Coman <dan.coman@trisoft.ro>
2016-10-07 15:29:55 +0300

    Unit todo WorkPackageProjectCost

---
+++ src/AppBundle/Form/Todo/CreateType.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace AppBundle\Form\Todo;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use AppBundle\Entity\Todo;
+use AppBundle\Entity\User;
+use AppBundle\Entity\Project;
+use AppBundle\Entity\Meeting;
+use AppBundle\Entity\Status;
+
+class CreateType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('project', EntityType::class, [
+                'class' => Project::class,
+                'choice_label' => 'name',
+            ])
+            ->add('meeting', EntityType::class, [
+                'class' => Meeting::class,
+                'choice_label' => 'name',
+            ])
+            ->add('title', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'validation.constraints.todo.title.not_blank',
+                    ]),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'validation.constraints.todo.description.not_blank',
+                    ]),
+                ],
+            ])
+            ->add('responsibility', EntityType::class, [
+                'class' => User::class,
+                'choice_label' => 'username',
+            ])
+            ->add('date', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+            ])
+            ->add('dueDate', DateType::class, [
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+            ])
+            ->add('status', EntityType::class, [
+                'class' => Status::class,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Todo::class,
+        ]);
+    }
+}
