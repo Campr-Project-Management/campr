0fd650f138bb1676d54c3499c4fc39d7a82ae7de
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2016-10-24 09:34:09 +0300

    DataTable for ProjectComplexity, ProjectCostType, ProjectDepartment. Layout fixed

--- src/AppBundle/Repository/ProjectDepartmentRepository.php
+++ src/AppBundle/Repository/ProjectDepartmentRepository.php
@@ -6,4 +6,49 @@ use Doctrine\ORM\EntityRepository;
 
 class ProjectDepartmentRepository extends EntityRepository
 {
+    /**
+     * Used to retrieve information for data table.
+     *
+     * New fields to be added to WHERE clause where the $key should be searched for.
+     *
+     * @param string|null $key
+     * @param string|null $field
+     * @param string|null $order
+     * @param int         $offset
+     * @param int         $limit
+     *
+     * @return array
+     */
+    public function findByKeyAndField($key, $field, $order, $offset, $limit)
+    {
+        $qb = $this->createQueryBuilder('q');
+
+        if (isset($key)) {
+            $qb = $qb
+                ->where('q.name LIKE CONCAT(\'%\', :key, \'%\')')
+                ->setParameter('key', $key)
+            ;
+        }
+
+        if (isset($field) && isset($order)) {
+            $qb = $qb->orderBy('q.'.$field, $order);
+        }
+
+        return $qb
+            ->setFirstResult($offset)
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+
+    public function countTotal()
+    {
+        return $this
+            ->createQueryBuilder('q')
+            ->select('COUNT(q.id)')
+            ->getQuery()
+            ->getSingleScalarResult()
+        ;
+    }
 }
