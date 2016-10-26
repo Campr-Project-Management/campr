07de39af10a1e07768b650ae6a27d51c2cb93e9b
Dan Coman <dan.coman@trisoft.ro>
2016-10-26 07:25:42 +0300

    Data table added to entities.

--- src/AppBundle/Repository/RaciRepository.php
+++ src/AppBundle/Repository/RaciRepository.php
@@ -2,8 +2,41 @@
 
 namespace AppBundle\Repository;
 
-use Doctrine\ORM\EntityRepository;
-
-class RaciRepository extends EntityRepository
+class RaciRepository extends BaseRepository
 {
+    public function findByWithLike(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+    {
+        $qb = $this
+            ->createQueryBuilder('q')
+            ->innerJoin('q.workPackage', 'wp')
+        ;
+
+        foreach ($criteria as $key => $value) {
+            if (empty($value)) {
+                continue;
+            }
+            $qb->andWhere(
+                $qb->expr()->like(
+                    'wp.'.$key,
+                    $qb->expr()->literal('%'.$value.'%')
+                )
+            );
+        }
+
+        if ($orderBy) {
+            foreach ($orderBy as $key => $value) {
+                $qb->orderBy('wp.'.$key, $value);
+            }
+        }
+
+        if ($limit) {
+            $qb->setMaxResults($limit);
+        }
+
+        if ($offset) {
+            $qb->setFirstResult($offset);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
 }
--- src/AppBundle/Repository/WorkPackageProjectWorkCostTypeRepository.php
+++ src/AppBundle/Repository/WorkPackageProjectWorkCostTypeRepository.php
@@ -2,8 +2,6 @@
 
 namespace AppBundle\Repository;
 
-use Doctrine\ORM\EntityRepository;
-
-class WorkPackageProjectWorkCostTypeRepository extends EntityRepository
+class WorkPackageProjectWorkCostTypeRepository extends BaseRepository
 {
 }
