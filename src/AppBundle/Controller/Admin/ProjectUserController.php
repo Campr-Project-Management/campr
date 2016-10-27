452ee429ae902b68f783ae9c20b3ee50df4823c6
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2016-10-27 14:05:38 +0300

    Fixing Entities Repo

--- src/AppBundle/Controller/Admin/ProjectUserController.php
+++ src/AppBundle/Controller/Admin/ProjectUserController.php
@@ -56,7 +56,7 @@ class ProjectUserController extends Controller
     {
         $requestParams = $request->request->all();
         $dataTableService = $this->get('app.service.data_table');
-        $response = $dataTableService->paginate(ProjectUser::class, $requestParams);
+        $response = $dataTableService->paginate(ProjectUser::class, 'username', $requestParams);
 
         return new JsonResponse($response);
     }
