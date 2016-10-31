2ba144bd158b192a8f7d491d5474e8b92cc028f0
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2016-10-31 13:02:04 +0200

    Added user-change-password command

---
+++ src/AppBundle/Command/UserChangePasswordCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace AppBundle\Command;
+
+use AppBundle\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class UserChangePasswordCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('tss:app:user-change-password')
+            ->addArgument('email', InputArgument::REQUIRED, 'The email of the user')
+            ->addArgument('password', InputArgument::REQUIRED, 'The password to set to the user')
+            ->addOption('role', null, InputOption::VALUE_IS_ARRAY | InputOption::VALUE_OPTIONAL, 'Roles to set to the user')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+        $roles = $input->getOption('role');
+
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $user = $em->getRepository(User::class)->findOneBy(['email' => $email]);
+
+        if (!$user) {
+            $output->writeln(sprintf(
+                '<error>Unable find user with email "%s".</error>',
+                $email
+            ));
+
+            return -1;
+        }
+
+        $encoder = $this->getContainer()->get('security.encoder_factory')->getEncoder($user);
+        $user->setPassword($encoder->encodePassword($password, $user->getSalt()));
+
+        if ($roles) {
+            $user->setRoles($roles);
+        }
+
+        $em->persist($user);
+        $em->flush();
+
+        $output->writeln('<info>User updated.</info>');
+    }
+}
