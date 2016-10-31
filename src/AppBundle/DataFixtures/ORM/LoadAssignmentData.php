6303615f7ab76d06fd3de3c20e03da64a65b49d2
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2016-10-31 18:23:34 +0200

    DataFixtures
    Data fixtures for Project entities
    
    DataFixtures for Meeting entites, Status and Unit
    
    Data fixtures for Risk entities, Impact, Todo, Schedule
    
    Data fixtures for Assignment, Calendar, Company, Day, Decision, Note, Raci, Timephase, WorkingTime, WorkPackage, WPPWCT

---
+++ src/AppBundle/DataFixtures/ORM/LoadAssignmentData.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Assignment;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadAssignmentData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $startedAt = new \DateTime();
+        $finishedAt = new \DateTime('+5 days');
+
+        for ($i = 1; $i <= 2; ++$i) {
+            $workPackage = $this->getReference('work-package'.$i);
+            $wppwct = $this->getReference('work-package-project-work-cost-type'.$i);
+
+            $assignment = (new Assignment())
+                ->setWorkPackage($workPackage)
+                ->setWorkPackageProjectWorkCostType($wppwct)
+                ->setMilestone($i * $i)
+                ->setStartedAt($startedAt)
+                ->setFinishedAt($finishedAt)
+            ;
+            $this->setReference('assignment'.$i, $assignment);
+            $manager->persist($assignment);
+
+            $finishedAt->add(new \DateInterval('P5D'));
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 4;
+    }
+}
---
+++ src/AppBundle/DataFixtures/ORM/LoadWorkPackageProjectWorkCostTypeData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\WorkPackageProjectWorkCostType;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadWorkPackageProjectWorkCostTypeData extends AbstractFixture implements OrderedFixtureInterface
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 1; $i <= 2; ++$i) {
+            $workPackage = $this->getReference('work-package'.$i);
+            $projectWorkCostType = $this->getReference('project-work-cost-type'.$i);
+
+            $wppwct = (new WorkPackageProjectWorkCostType())
+                ->setName('work-package-project-work-cost-type'.$i)
+                ->setWorkPackage($workPackage)
+                ->setProjectWorkCostType($projectWorkCostType)
+            ;
+            $this->setReference('work-package-project-work-cost-type'.$i, $wppwct);
+            $manager->persist($wppwct);
+        }
+
+        $manager->flush();
+    }
+
+    /**
+     * @return int
+     */
+    public function getOrder()
+    {
+        return 3;
+    }
+}
