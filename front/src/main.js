b1120aafd45b3370ceed63cc8239fd210c3d70f9
ionut.moldovan <ionut.moldovan@trisoft.ro>
2016-12-07 09:55:48 +0200

    init vue + vue-router + main components + sidebar

--- front/src/main.js
+++ front/src/main.js
@@ -1,9 +1,40 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import 'expose?$!expose?jQuery!jquery'
+import 'normalise.scss'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
 import App from './App'
+import Dashboard from './components/Dashboard'
+import Projects from './components/Projects'
+import Tasks from './components/Tasks'
 
-/* eslint-disable no-new */
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: Dashboard
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: Projects
+  },
+  {
+    path: '/tasks',
+    name: 'tasks',
+    component: Tasks
+  }
+]
+
+const router = new VueRouter({
+  routes // short for routes: routes
+})
+
 new Vue({
-  el: '#app',
+  router,
   template: '<App/>',
   components: { App }
-})
+}).$mount('#app')
