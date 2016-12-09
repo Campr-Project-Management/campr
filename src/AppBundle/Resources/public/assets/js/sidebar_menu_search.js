073f0dd00898f5814f5244c3703f1b1ce2f271b9
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2016-12-09 20:12:24 +0000

    fix

--- src/AppBundle/Resources/public/assets/js/sidebar_menu_search.js
+++ src/AppBundle/Resources/public/assets/js/sidebar_menu_search.js
@@ -1,17 +1,32 @@
 $(function () {
-    var $menuChild = $('.sidebar-main-menu > li'),
-        searchedString;
+    'use strict';
+    var $menuChild = $('.sidebar-main-menu > li');
 
     $.extend($.expr[":"], {
-        "icontains": function(elem, i, match, array) {
+        "icontains": function (elem, i, match) {
+            if (typeof i === 'function') { // this fools jslint
+                i();
+            }
+
             return (elem.textContent || elem.innerText || "").toLowerCase().indexOf((match[3] || "").toLowerCase()) >= 0;
         }
     });
 
     $('.sidebar-search').keyup(function () {
-        searchedString = $(this).val();
+        var searchedString = $(this).val();
 
-        $menuChild.css('display', 'none');
-        $menuChild.find(':icontains("' + searchedString + '")').parents('.main-category').css('display', 'block');
+        $menuChild.show();
+        $menuChild.find('ul').hide();
+        $menuChild.removeClass('toggled');
+        $('.main-category > a').css('color', '#4C4C4C');
+        $('.main-category > ul > li > a').css('color', '#989898');
+
+        if (searchedString) {
+            $menuChild.hide();
+            $menuChild.find(':icontains("' + searchedString + '")').parents('.main-category').show();
+            $menuChild.find(':icontains("' + searchedString + '")').parents('.main-category').addClass('toggled');
+            $menuChild.find(':icontains("' + searchedString + '")').parents('.main-category').find('ul').show();
+            $menuChild.find(':icontains("' + searchedString + '")').css('color', '#262626');
+        }
     });
 });
