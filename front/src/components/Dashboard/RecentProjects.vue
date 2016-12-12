f1c29a20cafe2501ae892ae76a8ed0f3ffcf8253
ionut.moldovan <ionut.moldovan@trisoft.ro>
2016-12-12 17:46:21 +0200

    setup vuex + navigation + my recent projects

---
+++ front/src/components/Dashboard/RecentProjects.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="projects-header">
+      <h1>My Recent Projects</h1>
+      <task-filters></task-filters>
+    </div>
+    <div class="recent-projects">
+      <project-box v-for="project in projects"></project-box>
+    </div>
+  </div>
+</template>
+
+<script>
+import TaskFilters from './TaskFilters'
+import ProjectBox from '../ProjectBox'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+  components: {
+    TaskFilters,
+    ProjectBox
+  },
+  methods: mapActions(['getProjects']),
+  created () {
+    this.getProjects()
+  },
+  computed: mapGetters({
+    projects: 'projects'
+  })
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  @import '../../css/_variables';
+
+  .projects-header {
+    display: flex;
+    justify-content: space-between;
+
+    h1 {
+      margin: 0;
+      text-transform: uppercase;
+      font-size: 20px;
+      line-height: 80px;
+    }
+  }
+
+  .recent-projects {
+    display: flex;
+    flex-flow: row wrap;
+    margin: -14px -13px;
+  }
+</style>
--- front/src/main.js
+++ front/src/main.js
@@ -1,40 +1,18 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
 import 'expose?$!expose?jQuery!jquery'
 import 'normalise.scss'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
+import router from './router'
+import store from './store'
+import {sync} from 'vuex-router-sync'
 import App from './App'
-import Dashboard from './components/Dashboard'
-import Projects from './components/Projects'
-import Tasks from './components/Tasks'
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/dashboard',
-    name: 'dashboard',
-    component: Dashboard
-  },
-  {
-    path: '/projects',
-    name: 'projects',
-    component: Projects
-  },
-  {
-    path: '/tasks',
-    name: 'tasks',
-    component: Tasks
-  }
-]
-
-const router = new VueRouter({
-  routes // short for routes: routes
-})
+sync(store, router)
 
 new Vue({
   router,
+  store,
   template: '<App/>',
-  components: { App }
+  components: {App}
 }).$mount('#app')
