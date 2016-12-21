f5e03bf0d9283ca7a8797deccb9f4398956b2d80
ionut.moldovan <ionut.moldovan@trisoft.ro>
2016-12-21 12:15:14 +0200

    Projects page & Tasks page (2)

--- front/src/components/_common/_charts/BarChart.vue
+++ front/src/components/_common/_charts/BarChart.vue
@@ -1,53 +1,36 @@
 <template>
-  <div :data-percentage="percentage" class="chart relative">
-      <svg viewBox="0 0 100 100">
-          <circle cx="50" cy="50" r="49" class="empty" stroke-width="1" fill="transparent"/>
-          <circle cx="50" cy="50" r="49" class="full" stroke-width="1" fill="transparent"/>
-      </svg>
-      <div class="text">
-          <p class="title">{{ title }}</p>
-          <p class="flex">
-              <span class="percentage">0</span>
-              <span class="percentage-sign flex-end">%</span>
-          </p>
-      </div>
-  </div>
+    <div class="progress-line" :id="'chart' + _uid" :data-number="percentage" v-bind:class="{ 'danger-col-bg': status === 'NOT_STARTED' }">
+        <div class="progress-area right flex-end">
+            <p class="title-right">{{ titleRight }}</p>
+            <p class="percentage">
+                <span class="number">0</span>
+                <span class="percentage-sign">%</span>
+            </p>
+        </div>
+        <p v-show="titleLeft" class="title-left">{{ titleLeft }}</p>
+        <div class="filled" v-bind:class="{ finished: status === 'FINISHED' }"></div>
+    </div>
 </template>
 
 <script>
 export default {
-    props: ['percentage', 'title'],
+    props: ['percentage', 'status', 'titleLeft', 'titleRight'],
     mounted() {
-        const progressBar = window.$('.chart');
+        const $this = window.$('#chart' + this._uid);
         let speed = 1000;
 
-        progressBar.each(function(i, elem) {
-            const $this = window.$(elem);
-            const $percentageNumber = $this.find('.percentage');
-            const animatedCircle = $this.find('.full');
-            const percentage = $this.data('percentage');
-            const c = Math.PI*(animatedCircle.attr('r')*2);
+        const $percentageNumber = $this.find('.number');
 
-            let strokeDasharray = 0;
-            let pct = percentage/100*c;
-            let animation = setInterval(function() {
-                strokeDasharray++;
+        $this.find('.filled')
+        .animate({'width': $this.data('number') + '%'}, speed);
 
-                animatedCircle[0]
-                  .style.strokeDasharray = strokeDasharray + ', 10000';
-                if (strokeDasharray >= pct) {
-                    clearInterval(animation);
-                };
-            }, 1);
-
-            window.$({Counter: 0})
-                .animate({Counter: $this.data('percentage')}, {
-                    duration: speed,
-                    step: function() {
-                        $percentageNumber.text(this.Counter.toFixed(2));
-                    },
-                });
-        });
+        window.$({Counter: 0})
+            .animate({Counter: $this.data('number')}, {
+                duration: speed,
+                step: function() {
+                    $percentageNumber.text(Math.ceil(this.Counter));
+                },
+            });
     },
 };
 </script>
@@ -57,50 +40,66 @@ export default {
   @import '../../../css/_common';
   @import '../../../css/_variables.scss';
 
-  .chart {
-    font-size: 22px;
+  .progress-area {
+    letter-spacing: 0.7px;
+    right: 0;
+    position: absolute;
+    top: -60px;
 
-    svg {
-      display: block;
-      width: 100%;
-      transform: rotate(-90deg);
+    &.right {
+      width: 60px;
     }
 
-    .empty {
-      stroke: $mainColor;
+    .title-right {
+      font-size: 8px;
+      text-transform: uppercase;
+      font-weight: 600;
+      min-height: 11px;
     }
 
-    .full {
-      stroke: $secondColor;
-      stroke-dasharray: 0, 10000;
+    .percentage {
+      color: $middleColor;
+      margin-top: 2px;
+      display: flex;
     }
 
-    .text {
-      position: absolute;
-      left: 50%;
-      top: 50%;
-      padding: 0;
-      margin: 0;
-      transform: translate(-50%, -50%);
-      color: rgb(216, 218, 229);
+    .number {
+      color: $lighterColor;
+      font-size: 32px;
+      height: 30px;
+      line-height: 35px;
+      min-width: 55px;
+      text-align: right;
+      font-weight: 700;
+    }
 
-      .title {
-        text-transform: uppercase;
-        font-size: 9px;
-        text-align: center;
+    .percentage-sign {
+      align-self: flex-end;
+    }
+  }
+
+  .progress-line {
+    margin-top: 13px;
+    width: 100%;
+    height: 8px;
+    background: $mainColor;
+    position: relative;
+
+    .filled {
+      width: 0;
+      background: $middleColor;
+      height: 100%;
+
+      &.finished {
+        background: $secondColor;
       }
-
-      .percentage {
-        font-weight: 700;
-        font-size: 34px;
-        line-height: 38px;
-        height: 33px;
-      }
-
-      .percentage-sign {
-        color: $middleColor;
-        font-size: 12px;
-      }
     }
+  }
+
+  .title-left {
+    position: absolute;
+    top: -32px;
+    font-size: 9px;
+    text-transform: uppercase;
   }
 </style>
