d4a6e1ca5902d106abd1b7097f42d7169784165b
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2016-12-22 15:01:50 +0200

    Team user management for users
    Invite team user
    
    fixed tests
    
    New tests and fixed tests
    
    Validators added

--- src/MainBundle/Tests/Controller/Admin/TeamControllerTest.php
+++ src/MainBundle/Tests/Controller/Admin/TeamControllerTest.php
@@ -23,12 +23,19 @@ class TeamControllerTest extends BaseController
 
         $this->assertContains('id="create_name"', $crawler->html());
         $this->assertContains('name="create[name]"', $crawler->html());
+<<<<<<< HEAD
         $this->assertContains('id="create_logoFile_file"', $crawler->html());
         $this->assertContains('name="create[logoFile][file]"', $crawler->html());
         $this->assertContains('id="create_description"', $crawler->html());
         $this->assertContains('name="create[description]"', $crawler->html());
         $this->assertContains('id="create_enabled"', $crawler->html());
         $this->assertContains('name="create[enabled]"', $crawler->html());
+=======
+        $this->assertContains('id="create_enabled"', $crawler->html());
+        $this->assertContains('name="create[enabled]"', $crawler->html());
+        $this->assertContains('id="create_description"', $crawler->html());
+        $this->assertContains('name="create[description]"', $crawler->html());
+>>>>>>> Team user management for users
         $this->assertContains('type="submit"', $crawler->html());
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
@@ -46,12 +53,20 @@ class TeamControllerTest extends BaseController
         $form = $crawler->filter('#create-team')->first()->form();
         $crawler = $this->client->submit($form);
 
+<<<<<<< HEAD
         $this->assertContains('Please enter a team name!', $crawler->html());
+=======
+        $this->assertContains('Team name should not be blank.', $crawler->html());
+>>>>>>> Team user management for users
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
 
+<<<<<<< HEAD
     public function testFormFileValidationOnCreatePage()
+=======
+    public function testUniqueNameOnCreatePage()
+>>>>>>> Team user management for users
     {
         $this->user = $this->createUser('testuser', 'testuser@trisoft.ro', 'Password1', ['ROLE_SUPER_ADMIN']);
         $this->login($this->user);
@@ -61,6 +76,7 @@ class TeamControllerTest extends BaseController
         $crawler = $this->client->request(Request::METHOD_GET, '/admin/team/create');
 
         $form = $crawler->filter('#create-team')->first()->form();
+<<<<<<< HEAD
         $form['create[name]'] = 'tname';
 
         $container = self::$kernel->getContainer();
@@ -86,6 +102,12 @@ class TeamControllerTest extends BaseController
         $crawler = $this->client->submit($form);
 
         $this->assertContains('Uploaded file must be an image!', $crawler->html());
+=======
+        $form['create[name]'] = 'team_1';
+        $crawler = $this->client->submit($form);
+
+        $this->assertContains('Team name already used.', $crawler->html());
+>>>>>>> Team user management for users
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
@@ -100,7 +122,10 @@ class TeamControllerTest extends BaseController
 
         $form = $crawler->filter('#create-team')->first()->form();
         $form['create[name]'] = 'tname';
+<<<<<<< HEAD
         $form['create[description]'] = 'descript';
+=======
+>>>>>>> Team user management for users
 
         $this->client->submit($form);
         $this->assertTrue($this->client->getResponse()->isRedirect());
@@ -113,7 +138,10 @@ class TeamControllerTest extends BaseController
             ->getRepository(Team::class)
             ->findOneBy([
                 'name' => 'tname',
+<<<<<<< HEAD
                 'description' => 'descript',
+=======
+>>>>>>> Team user management for users
             ])
         ;
         $this->em->remove($team);
@@ -128,7 +156,10 @@ class TeamControllerTest extends BaseController
 
         $team = (new Team())
             ->setName('team_test')
+<<<<<<< HEAD
             ->setDescription('description team test')
+=======
+>>>>>>> Team user management for users
         ;
         $this->em->persist($team);
         $this->em->flush();
@@ -149,6 +180,7 @@ class TeamControllerTest extends BaseController
         /** @var Crawler $crawler */
         $crawler = $this->client->request(Request::METHOD_GET, '/admin/team/1/edit');
 
+<<<<<<< HEAD
         $this->assertContains('id="edit_name"', $crawler->html());
         $this->assertContains('name="edit[name]"', $crawler->html());
         $this->assertContains('id="edit_slug"', $crawler->html());
@@ -159,6 +191,14 @@ class TeamControllerTest extends BaseController
         $this->assertContains('name="edit[description]"', $crawler->html());
         $this->assertContains('id="edit_enabled"', $crawler->html());
         $this->assertContains('name="edit[enabled]"', $crawler->html());
+=======
+        $this->assertContains('id="create_name"', $crawler->html());
+        $this->assertContains('name="create[name]"', $crawler->html());
+        $this->assertContains('id="create_enabled"', $crawler->html());
+        $this->assertContains('name="create[enabled]"', $crawler->html());
+        $this->assertContains('id="create_description"', $crawler->html());
+        $this->assertContains('name="create[description]"', $crawler->html());
+>>>>>>> Team user management for users
         $this->assertContains('type="submit"', $crawler->html());
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
@@ -174,13 +214,35 @@ class TeamControllerTest extends BaseController
         $crawler = $this->client->request(Request::METHOD_GET, '/admin/team/1/edit');
 
         $form = $crawler->filter('#edit-team')->first()->form();
+<<<<<<< HEAD
         $form['edit[name]'] = '';
         $form['edit[slug]'] = 'team-2';
+=======
+        $form['create[name]'] = '';
+>>>>>>> Team user management for users
 
         $crawler = $this->client->submit($form);
 
-        $this->assertContains('Please enter a team name!', $crawler->html());
-        $this->assertContains('The provided slug is already used!', $crawler->html());
+        $this->assertContains('Team name should not be blank.', $crawler->html());
+
+        $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
+    }
+
+    public function testUniqueNameOnEditPage()
+    {
+        $this->user = $this->createUser('testuser', 'testuser@trisoft.ro', 'Password1', ['ROLE_SUPER_ADMIN']);
+        $this->login($this->user);
+        $this->assertNotNull($this->user, 'User not found');
+
+        /** @var Crawler $crawler */
+        $crawler = $this->client->request(Request::METHOD_GET, '/admin/team/2/edit');
+
+        $form = $crawler->filter('#edit-team')->first()->form();
+        $form['create[name]'] = 'team_1';
+
+        $crawler = $this->client->submit($form);
+
+        $this->assertContains('Team name already used.', $crawler->html());
 
         $this->assertSame(Response::HTTP_OK, $this->client->getResponse()->getStatusCode());
     }
---
+++ src/MainBundle/Validator/Constraints/TeamMember/SelfInviteValidator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace MainBundle\Validator\Constraints\TeamMember;
+
+use AppBundle\Entity\User;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class SelfInviteValidator extends ConstraintValidator
+{
+    private $em;
+
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function validate($value, Constraint $constraint)
+    {
+        $user = $this
+            ->em
+            ->getRepository(User::class)
+            ->findOneBy([
+                'email' => $value,
+            ])
+        ;
+
+        if ($user === $constraint->user) {
+            $this
+                ->context
+                ->buildViolation($constraint->message)
+                ->addViolation();
+        }
+    }
+}
