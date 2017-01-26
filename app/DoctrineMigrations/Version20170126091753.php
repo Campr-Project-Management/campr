d6f29f47ec7fd36fc4cc70df4be9ee0dddf202e9
Dan Coman <dan.coman@trisoft.ro>
2017-01-26 14:15:32 +0200

    User changes and sync.

---
+++ app/DoctrineMigrations/Version20170126091753.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170126091753 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user ADD facebook VARCHAR(256) DEFAULT NULL, ADD twitter VARCHAR(256) DEFAULT NULL, ADD instagram VARCHAR(256) DEFAULT NULL, ADD gplus VARCHAR(256) DEFAULT NULL, ADD linked_in VARCHAR(256) DEFAULT NULL, ADD medium VARCHAR(256) DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE user DROP facebook, DROP twitter, DROP instagram, DROP gplus, DROP linked_in, DROP medium');
+    }
+}
