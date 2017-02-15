ea5c26c5aa6d0163f6928c93a5c95abacecad43f
Cosmin Chirica <cosmin.chirica@trisoft.ro>
2017-02-15 06:56:25 +0000

    Contract API endpoints - changes

---
+++ src/AppBundle/Form/Contract/BaseCreateType.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace AppBundle\Form\Contract;
+
+use AppBundle\Entity\Contract;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class BaseCreateType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array                $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'required' => true,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'not_blank.name',
+                    ]),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('proposedStartDate', DateType::class, [
+                'required' => false,
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+            ])
+            ->add('proposedEndDate', DateType::class, [
+                'required' => false,
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+            ])
+            ->add('forecastStartDate', DateType::class, [
+                'required' => false,
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+            ])
+            ->add('forecastEndDate', DateType::class, [
+                'required' => false,
+                'widget' => 'single_text',
+                'format' => 'dd-MM-yyyy',
+            ])
+        ;
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Contract::class,
+        ]);
+    }
+}
