12360b1321b5e7a42ebd8605249194836293f155
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-04-03 00:02:59 +0300

    small fix for deploy

--- frontend/config/dev.env.js
+++ frontend/config/dev.env.js
@@ -3,13 +3,15 @@ let merge = require('webpack-merge'),
     prodEnv = require('./prod.env'),
     fs = require('fs');
 
-let devLocalConfigFile = __dirname + '/dev.local.js';
-if (fs.existsSync(devLocalConfigFile)) {
-    let devLocalConfig = require('./dev.local');
-    prodEnv = merge(prodEnv, devLocalConfig);
-} else {
-    console.log('dev.local.js is missing');
-    process.exit(-1);
+if (env.NODE_ENV !== 'production') {
+    let devLocalConfigFile = __dirname + '/dev.local.js';
+    if (fs.existsSync(devLocalConfigFile)) {
+        let devLocalConfig = require('./dev.local');
+        prodEnv = merge(prodEnv, devLocalConfig);
+    } else {
+        console.log('dev.local.js is missing');
+        process.exit(-1);
+    }
 }
 
 module.exports = merge(prodEnv, {
