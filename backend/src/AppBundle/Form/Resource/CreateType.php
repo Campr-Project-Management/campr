670a2f807e46167d3a8308b7b040a8abcb957b30
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-04-11 05:36:18 +0300

    #121

---
+++ backend/src/AppBundle/Form/Resource/CreateType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Form\Resource;
+
+use AppBundle\Entity\Resource;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name', TextType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Resource::class,
+            'csrf_protection' => false,
+        ]);
+    }
+}
