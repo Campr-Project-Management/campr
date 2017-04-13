fa840dc8ca1f9c14991f5fffc2471e361be81791
Dan Coman <dan.coman@trisoft.ro>
2017-04-13 19:00:21 +0300

    Fixes and custom unit

--- backend/src/AppBundle/Repository/UnitRepository.php
+++ backend/src/AppBundle/Repository/UnitRepository.php
@@ -4,4 +4,20 @@ namespace AppBundle\Repository;
 
 class UnitRepository extends BaseRepository
 {
+    public function findMaxSequence(Project $project = null)
+    {
+        $qb = $this
+            ->createQueryBuilder('u')
+            ->select('MAX(u.sequence)')
+        ;
+
+        if ($project) {
+            $qb
+                ->andWhere('u.project', ':project')
+                ->setParameter('project', $project)
+            ;
+        }
+
+        return $qb->getQuery()->getSingleScalarResult();
+    }
 }
