aa9f75b1c0b6a9b465093c9cc83b65cfa38724e6
Dan Coman <dan.coman@trisoft.ro>
2017-04-14 14:58:26 +0300

    Edit page and fixes

--- frontend/src/components/Projects/TaskManagement/Create.vue
+++ frontend/src/components/Projects/TaskManagement/Create.vue
@@ -9,7 +9,8 @@
                             <i class="fa fa-angle-left"></i>
                             Back to Task Management
                         </router-link>
-                        <h1>{{ message.create_new_task }}</h1>
+                        <h1 v-if="!isEdit">{{ message.create_new_task }}</h1>
+                        <h1 v-if="isEdit">{{ message.edit_task }} - {{ task.name }}</h1>
                     </div>
                     <a class="btn-rounded btn-auto btn-empty flex">
                         <span>{{ message.import_task }}</span>
@@ -42,7 +43,7 @@
                     <hr>
 
                     <!-- /// Task Schedule /// -->
-                    <schedule v-model="schedule" />
+                    <schedule v-model="schedule" v-bind:editSchedule="schedule" />
                     <!-- /// End Task Schedule /// -->
 
                     <hr class="double">
@@ -60,13 +61,13 @@
                     <hr class="double">
 
                     <!-- /// Task Details /// -->
-                    <task-details v-model="details" />
+                    <task-details v-model="details" v-bind:editDetails="details" />
                     <!-- /// End Task Details /// -->
 
                     <hr class="double">
 
                     <!-- /// SubTasks /// -->
-                    <subtasks v-model="subtasks" />
+                    <subtasks v-model="subtasks" v-bind:editSubtasks='subtasks' />
                     <!-- /// End SubTasks /// -->
 
                     <hr class="double">
@@ -86,7 +87,8 @@
                     <!-- /// Actions /// -->
                     <div class="flex flex-space-between">
                         <router-link :to="{name: 'project-task-management-list'}" class="btn-rounded btn-auto disable-bg">{{ button.cancel }}</router-link>
-                        <a v-on:click="createTask" class="btn-rounded btn-auto second-bg">{{ button.create_task }}</a>
+                        <a v-if="!isEdit" @click="createTask" class="btn-rounded btn-auto second-bg">{{ button.create_task }}</a>
+                        <a v-if="isEdit" @click="editExistingTask" class="btn-rounded btn-auto second-bg">{{ button.edit_task }}</a>
                     </div>
                     <!-- /// End Actions /// -->
                 </div>
@@ -110,7 +112,7 @@ import TaskDetails from './Create/Details';
 import Attachments from './Create/Attachments';
 import datepicker from 'vuejs-datepicker';
 import Switches from '../../3rdparty/vue-switches';
-import {mapActions} from 'vuex';
+import {mapGetters, mapActions} from 'vuex';
 import {createFormData} from '../../../helpers/task';
 
 export default {
@@ -131,7 +133,7 @@ export default {
         Attachments,
     },
     methods: {
-        ...mapActions(['createNewTask']),
+        ...mapActions(['createNewTask', 'getTaskById', 'editTask']),
         createTask: function() {
             let data = {
                 project: this.$route.params.id,
@@ -153,11 +155,90 @@ export default {
                 projectId: this.$route.params.id,
             });
         },
+        editExistingTask: function() {
+            let data = {
+                project: this.$route.params.id,
+                name: this.title,
+                type: 2,
+                description: this.$refs.description.getContent(),
+                schedule: this.schedule,
+                internalCosts: this.internalCosts,
+                externalCosts: this.externalCosts,
+                subtasks: this.subtasks,
+                planning: this.planning,
+                attachments: this.attachments,
+                details: this.details,
+                statusColor: this.statusColor,
+            };
+            this.editTask({
+                data: createFormData(data),
+                taskId: this.$route.params.taskId,
+            });
+        },
+    },
+    created() {
+        if (this.$route.params.taskId) this.getTaskById(this.$route.params.taskId);
+    },
+    computed: mapGetters({
+        task: 'task',
+    }),
+    watch: {
+        task(value) {
+            this.title = this.task.name;
+            this.$refs.description.setContent(this.task.content);
+            this.schedule = {
+                baseStartDate: new Date(this.task.scheduledStartAt),
+                baseEndDate: new Date(this.task.scheduledFinishAt),
+                forecastStartDate: new Date(this.task.forecastStartAt),
+                forecastEndDate: new Date(this.task.forecastFinishAt),
+                automatic: false,
+                successors: [],
+                predecessors: [],
+                durationInDays: 0,
+            };
+            this.statusColor = {
+                id: this.task.colorStatus,
+                name: this.task.colorStatusName,
+            };
+            this.details = {
+                assignee: this.task.responsibility ? {key: this.task.responsibility, label: this.task.responsibilityFullName} : '',
+                status: this.task.workPackageStatus ? {key: this.task.workPackageStatus, label: this.task.workPackageStatusName} : '',
+                labels: {},
+            };
+            let children = [];
+            this.task.children.map(function(child) {
+                children.push({
+                    description: child.name,
+                });
+            });
+            this.subtasks = children;
+            let internal = [];
+            let external = [];
+            // let external = {items: []};
+            this.task.costs.map(function(cost) {
+                if (cost.type === 0) {
+                    internal.push(
+                        {resource: cost.resource ? cost.resource : '', qty: cost.quantity, days: cost.duration, rate: cost.rate}
+                    );
+                } else {
+                    external.push(
+                        {
+                            description: cost.name, qty: cost.quantity, unitRate: cost.rate,
+                            capex: cost.expenseType === 0 ? 1 : 0, opex: cost.expenseType === 1 ? 1 : 0, customUnit: '', selectedUnit: cost.unit.name,
+                        }
+                    );
+                }
+            });
+            this.internalCosts.items = internal;
+            this.externalCosts.items = external;
+        },
     },
     data() {
         return {
+            isEdit: this.$route.params.taskId,
             message: {
                 create_new_task: this.translate('message.create_new_task'),
+                edit_task: this.translate('message.edit_task'),
                 import_task: this.translate('message.import_task'),
                 task_details: this.translate('message.task_details'),
             },
@@ -168,6 +249,7 @@ export default {
             button: {
                 cancel: this.translate('button.cancel'),
                 create_task: this.translate('button.create_task'),
+                edit_task: this.translate('button.edit_task'),
             },
             schedule: {
                 baseStartDate: new Date(),
@@ -180,8 +262,16 @@ export default {
                 durationInDays: 0,
             },
             title: '',
-            internalCosts: [],
-            externalCosts: {},
+            internalCosts: {
+                items: [],
+                forecast: 0,
+                actual: 0,
+            },
+            externalCosts: {
+                items: [],
+                forecast: 0,
+                actual: 0,
+            },
             subtasks: [],
             planning: {},
             attachments: [],
