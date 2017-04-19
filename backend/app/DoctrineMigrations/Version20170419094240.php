5ac13bb4d24bf1e1db6927b12a71f5690a15a4c7
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-04-19 12:44:17 +0300

    small fix

---
+++ backend/app/DoctrineMigrations/Version20170419094240.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170419094240 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('UPDATE work_package SET duration = 0 WHERE duration IS NULL');
+        $this->addSql('ALTER TABLE work_package CHANGE duration duration INT DEFAULT 0 NOT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE work_package CHANGE duration duration VARCHAR(64) DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
