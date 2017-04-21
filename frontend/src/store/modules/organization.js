4c0473210be5800d0d309932a6684ad57a3046d6
root <cristian.dan@trisoft.ro>
2017-04-22 00:09:14 +0200

    members create wip

---
+++ frontend/src/store/modules/organization.js
@@ -0,0 +1,35 @@
+// import Vue from 'vue';
+// import * as types from '../mutation-types';
+// import router from '../../router';
+
+const state = {
+    members: [],
+    items: [],
+};
+
+const getters = {
+    items: state => state.items,
+    member: state => state.members,
+};
+
+const actions = {
+    /**
+     * Creates a new member in organization
+     * @param {function} commit
+     * @param {number} member
+     */
+    createNewOrganizationMember({commit}, member) {
+        // Vue.http
+        console.log('meber create', member);
+    },
+};
+
+const mutations = {
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations,
+};
