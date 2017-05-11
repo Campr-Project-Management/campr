6e10a4412d374d73c2c27f55ed90f706f3a9a007
Vasile Codrea <vasile.codrea@trisoft.ro>
2017-05-11 10:44:55 +0300

    Implement CR feedback
    Remove labels for old statuses

--- backend/app/DoctrineMigrations/Version20170510073333.php
+++ backend/app/DoctrineMigrations/Version20170510073333.php
@@ -16,14 +16,14 @@ class Version20170510073333 extends AbstractMigration
     public function up(Schema $schema)
     {
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
-        
-        $this->addSql('DELETE FROM `work_package_status`');
-        $this->addSql('INSERT INTO `work_package_status` (`id`, `name`, `sequence`, `visible`, `created_at`, `updated_at`)
+
+        $this->addSql('REPLACE INTO `work_package_status` (`id`, `name`, `sequence`, `visible`, `created_at`, `updated_at`)
             VALUES 
             (1, \'label.open\', 0, 1, \'2017-01-01\', \'2017-01-01\'),
             (2, \'label.pending\', 1, 1, \'2017-01-01\', \'2017-01-01\'),
             (3, \'label.ongoing\', 2, 1, \'2017-01-01\', \'2017-01-01\'),
-            (4, \'label.completed\', 3, 1, \'2017-01-01\', \'2017-01-01\')
+            (4, \'label.completed\', 3, 1, \'2017-01-01\', \'2017-01-01\'),
+            (5, \'label.closed\', -1, 0, \'2017-01-01\', \'2017-01-01\')
         ');
     }
 
@@ -35,6 +35,13 @@ class Version20170510073333 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('DELETE FROM `work_package_status`');
+        $this->addSql('REPLACE INTO `work_package_status` (`id`, `name`, `sequence`, `visible`, `created_at`, `updated_at`)
+            VALUES 
+            (1, \'label.todo\', 0, 1, \'2017-01-01\', \'2017-01-01\'),
+            (2, \'label.in_progress\', 1, 1, \'2017-01-01\', \'2017-01-01\'),
+            (3, \'label.code_review\', 2, 1, \'2017-01-01\', \'2017-01-01\'),
+            (4, \'label.finished\', 3, 1, \'2017-01-01\', \'2017-01-01\'),
+            (5, \'label.closed\', -1, 0, \'2017-01-01\', \'2017-01-01\')
+        ');
     }
 }
