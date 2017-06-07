bc6480f709c0cad42eb281ac75eb24c0aaf4341a
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-06-07 15:46:19 +0300

    gantt chart wip

---
+++ web/assets/dhtmlxGrantt/sources/locale/locale_es.js
@@ -0,0 +1,68 @@
+/*
+@license
+
+dhtmlxGantt v.4.1.0 Stardard
+This software is covered by GPL license. You also can obtain Commercial or Enterprise license to use it in non-GPL project - please contact sales@dhtmlx.com. Usage without proper license is prohibited.
+
+(c) Dinamenta, UAB.
+*/
+/*
+ @Autor Manuel Fernandez Panzuela - www.mfernandez.es
+
+ Update 30/10/2015:
+ Translation of new labels by Jorge Macias
+ https://disqus.com/by/disqus_bTuZk1voC7/
+ */
+gantt.locale = {
+	date: {
+		month_full: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
+		month_short: ["Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sep", "Oct", "Nov", "Dic"],
+		day_full: ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"],
+		day_short: ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"]
+	},
+	labels: {
+		dhx_cal_today_button: "Hoy",
+		day_tab: "Día",
+		week_tab: "Semana",
+		month_tab: "Mes",
+		new_event: "Nuevo evento",
+		icon_save: "Guardar",
+		icon_cancel: "Cancelar",
+		icon_details: "Detalles",
+		icon_edit: "Editar",
+		icon_delete: "Eliminar",
+		confirm_closing: "", //"Sus cambios se perderán, continuar ?"
+		confirm_deleting: "El evento se borrará definitivamente, ¿continuar?",
+		section_description: "Descripción",
+		section_time: "Período",
+		section_type:"Tipo",
+		/* grid columns */
+
+		column_text : "Tarea",
+		column_start_date : "Inicio",
+		column_duration : "Duración",
+		column_add : "",
+
+		/* link confirmation */
+		link: "Enlace",
+		confirm_link_deleting:"será borrada",
+		link_start: " (inicio)",
+		link_end: " (fin)",
+
+		type_task: "Tarea",
+		type_project: "Proyecto",
+		type_milestone: "Hito",
+
+
+		minutes: "Minutos",
+		hours: "Horas",
+		days: "Días",
+		weeks: "Semanas",
+		months: "Meses",
+		years: "Años",
+
+		/* message popup */
+		message_ok: "OK",
+		message_cancel: "Cancelar"
+	}
+};
