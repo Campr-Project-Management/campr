d9cbf52b20125f2058d13fdd34f05dc35cc7488f
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-06-12 14:11:05 +0300

    updated puids command

--- backend/src/AppBundle/Command/Update/PUIDsCommand.php
+++ backend/src/AppBundle/Command/Update/PUIDsCommand.php
@@ -4,44 +4,29 @@ namespace AppBundle\Command\Update;
 
 use AppBundle\Entity\Project;
 use AppBundle\Entity\WorkPackage;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\Query;
-use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class PUIDsCommand extends ContainerAwareCommand
+class PUIDsCommand extends BaseUpdateCommand
 {
-    /** @var OutputInterface */
-    private $output;
-
-    /** @var EntityManager */
-    private $em;
-
     protected function configure()
     {
         $this->setName('app:update:puids');
     }
 
-    protected function initialize(InputInterface $input, OutputInterface $output)
-    {
-        $this->output = $output;
-        $this->em = $this->getContainer()->get('doctrine.orm.default_entity_manager');
-    }
-
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $projects = $this->em->getRepository(Project::class)->findAll();
+        $projects = $this->getEm()->getRepository(Project::class)->findAll();
         if (!$projects || !count($projects)) {
             $output->writeln('<comment>No projects found.</comment>');
 
             return;
         }
-        $this->em->clear();
+        $this->getEm()->clear();
         // reset all PUIDs before updating
-        $this->em->beginTransaction();
+        $this->getEm()->beginTransaction();
         $this
-            ->em
+            ->getEm()
             ->createQuery(sprintf(
                 'UPDATE %s AS wp SET wp.puid = CONCAT(wp.id, :rand)',
                 WorkPackage::class
@@ -50,19 +35,27 @@ class PUIDsCommand extends ContainerAwareCommand
                 'rand' => random_int(0, time()),
             ])
         ;
-        $this->em->commit();
+        $this->getEm()->commit();
         foreach ($projects as $project) {
             $output->writeln(sprintf(
                 '<info>Processing: %s</info>',
                 $project
             ));
-            $this->updateWorkPackages($project->getId());
+            $this->updateWorkPackages($project);
         }
     }
 
-    private function updateWorkPackages(int $projectId, string $puidPrefix = '', int $workPackageParentId = null)
+    private function printWps(array $workPackages, $level = 0)
     {
-        $wps = $this->findWorkPackages($projectId, $workPackageParentId);
+        foreach ($workPackages as $wp) {
+            echo str_repeat('  ', $level), "ID: {$wp['id']} / Type: {$wp['type']} / Puid: {$wp['puid']}, Name: {$wp['name']}", PHP_EOL;
+            $this->printWps($wp['children'], $level + 1);
+        }
+    }
+
+    private function updateWorkPackages(Project $project, string $puidPrefix = '', array $workPackageParent = null)
+    {
+        $wps = $this->getWorkPackages($project, $workPackageParent, false, true);
         $c = 0;
         foreach ($wps as $wp) {
             ++$c;
@@ -74,44 +67,17 @@ class PUIDsCommand extends ContainerAwareCommand
                 'puid' => $puidPrefix ? $puidPrefix.'.'.$c : $c,
                 'id' => $wp['id'],
             ];
+            $this->output->writeln(sprintf(
+                '<comment>Setting puid %s for WP %s</comment>',
+                $params['puid'],
+                $wp['name']
+            ));
             $this
-                ->em
+                ->getEm()
                 ->createQuery($dql)
                 ->execute($params)
             ;
-            $this->updateWorkPackages($projectId, $c, (int) $wp['id']);
+            $this->updateWorkPackages($project, $params['puid'], $wp);
         }
-    }
-
-    private function findWorkPackages(int $projectId, int $workPackageId = null)
-    {
-        $dql = sprintf(
-            '
-                SELECT
-                    wp.id,
-                    wp.puid,
-                    pwp.puid AS parent_puid
-                FROM %1$s AS wp
-                LEFT JOIN %1$s AS pwp WITH pwp.id = wp.parent
-                WHERE wp.project = :project
-                AND wp.parent %2$s',
-            WorkPackage::class,
-            $workPackageId
-                ? '= :parent'
-                : 'IS NULL'
-        );
-        $params = [
-            'project' => $projectId,
-        ];
-        if ($workPackageId) {
-            $params['parent'] = $workPackageId;
-        }
-
-        return $this
-            ->em
-            ->createQuery($dql)
-            ->setParameters($params)
-            ->getResult(Query::HYDRATE_ARRAY)
-        ;
     }
 }
