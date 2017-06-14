269b41838ca8fb46d3c07a7fdcaa06d70828f0b1
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-06-14 03:11:44 +0300

    gantt chart updates

--- frontend/src/components/Projects/Gantt.vue
+++ frontend/src/components/Projects/Gantt.vue
@@ -10,6 +10,9 @@
 <script>
 import 'dhtmlx-gantt';
 import {mapActions, mapGetters} from 'vuex';
+import router from '../../router';
+
+const DAY_IN_MILISECONDS = 86400000;
 
 export default {
     name: 'gantt',
@@ -19,82 +22,269 @@ export default {
     methods: {
         ...mapActions(['getGanttData']),
     },
-    computed: {
-        ...mapGetters(['ganttData', 'ganttDataFormatted', 'ganttDataLinks']),
-        // @NOTE: This is a WIP, it's only here as POC
-        ganttDataFormatted() {
-            return this.ganttData.map((item, key) => {
-                // {"id":11, "text":"Project #1", "start_date":"28-03-2013", "duration":"11", "progress": 0.6, "open": true},
-                // scheduledStartAt
-                // scheduledFinishAt
-                // forecastStartAt
-                // forecastFinishAt
-                // actualStartAt
-                // actualFinishAt
-//            console.log((item.actualStartAt || item.forecastFinishAt || item.scheduledStartAt) || '28-03-2013');
-                let dt = new Date();
-                dt.setTime(dt.getTime() + (24 * 3600 * 1000 * key));
-                let out = {
-                    id: item.id,
-                    text: item.puid + ' ' + item.name,
-//                    start_date: (item.actualStartAt || item.forecastFinishAt || item.scheduledStartAt) || '28-03-2017',
-                    start_date: dt.getDate() + '-' + (dt.getMonth() + 1) + '-' + dt.getFullYear(),
-                    duration: 5,
-                    progress: .5,
-                    open: true,
-                };
-                if (item.parent) {
-                    out.parent = item.parent;
-                }
-                return out;
-            });
-        },
-        ganttDataLinks() {
-            const out = [];
-            // {"id":"1","source":"1","target":"2","type":"1"},
-            this.ganttData.forEach((item) => {
-                if (item.parent) {
-                    out.push({
-                        id: out.length + 1,
-                        source: item.parent,
-                        target: item.id,
-//                        type: item.type,
-//                        type: item.parent
-//                            ? this.ganttData.filter((itm) => itm.id === item.parent)[0].type
-//                            : item.type,
-                        type: 0,
-                    });
-                }
-            });
-            return out;
-        },
+    mounted() {
+        gantt.init(this.$refs.gantt_chart);
     },
     watch: {
         ganttData(value) {
             if (value && value.length) {
-                this.$refs['gantt_chart'].innerHTML = '';
-                gantt.init('gantt_chart');
-//                console.log({
-//                    data: this.ganttDataFormatted,
-//                    links: this.ganttDataLinks,
-//                });
+                // config
+                gantt.config.auto_scheduling_descendant_links = false;
+//                gantt.config.buttons_left = [];
+//                gantt.config.buttons_right = [];
+//                gantt.config.details_on_create = false;
+//                gantt.config.details_on_dblclick = false;
+//                gantt.config.show_unscheduled = false; // woot?
+                gantt.config.show_unscheduled = true;
+
+                // init!
+                gantt.init(this.$refs.gantt_chart);
                 gantt.parse({
                     data: this.ganttDataFormatted,
                     links: this.ganttDataLinks,
                 });
+
+                // this mostly helps in dev mode to prevent events from popping up twice
+                gantt.detachAllEvents();
+
+                // disable link double clicks
+                gantt.attachEvent('onLinkDblClick', (id, e) => false);
+
+                // instead of opening default edit, confirm if user wants to go to the edit page
+                gantt.attachEvent('onTaskDblClick', (id, e) => {
+                    id = parseInt(id, 10);
+                    const task = this.ganttData.filter(item => item.id === id)[0];
+                    const types = [
+                        'phase',
+                        'milestone',
+                        'task',
+                    ];
+
+                    gantt.confirm(this.translate('message.edit_' + types[task.type]) + ' <strong>' + task.name + '</strong>?', answer => {
+                        if (answer) {
+                            router.push({
+                                name: 'project-task-management-edit',
+                                params: {
+                                    'projectId': task.name,
+                                    'taskId': id,
+                                },
+                            });
+                        }
+                    });
+                    return false;
+                });
+
+                gantt.attachEvent('onAfterTaskDrag', (id, mode) => {
+//                    switch (mode) {
+//                    case gantt.config.drag_mode.ignore:
+//                        console.log(mode);
+//                        break;
+//                    case gantt.config.drag_mode.move:
+//                        console.log(mode);
+//                        break;
+//                    case gantt.config.drag_mode.progress:
+//                        console.log(mode);
+//                        break;
+//                    case gantt.config.drag_mode.resize:
+//                        console.log(mode);
+//                        break;
+//                    }
+//                    var task = gantt.getTask(id);
+//                    if(mode == gantt.config.drag_mode.progress){
+//                        var pr = Math.floor(task.progress * 100 * 10)/10;
+//                        gantt.message(task.text + " is now " + pr + "% completed!");
+//                    }else{
+//                        var convert = gantt.date.date_to_str("%H:%i, %F %j");
+//                        var s = convert(task.start_date);
+//                        var e = convert(task.end_date);
+//                        gantt.message(task.text + " starts at " + s + " and ends at " + e);
+//                    }
+                });
+                gantt.attachEvent('onBeforeTaskChanged', (id, mode, oldEvent) => {
+//                    var task = gantt.getTask(id);
+//                    if(mode == gantt.config.drag_mode.progress){
+//                        if(task.progress < old_event.progress){
+//                            gantt.message(task.text + " progress can't be undone!");
+//                            return false;
+//                        }
+//                    }
+//                    return true;
+                    const task = gantt.getTask(id);
+                    const progress = parseInt(task.progress * 100, 10);
+                    this
+                        .$http
+                        .patch(
+                            Routing.generate('app_api_workpackage_edit', {id}),
+                            {progress}
+                        )
+                        .then(
+                            (response) => {
+                                gantt.message(this.translate('message.saved'));
+                            },
+                            (response) => {
+                                if (response.status === 202) {
+                                    gantt.message(this.translate('message.saved'));
+                                } else {
+                                    task.progress = oldEvent.progress;
+                                    gantt.updateTask(id);
+                                    gantt.message(this.translate('message.unable_to_save'));
+                                }
+                            }
+                        )
+                    ;
+
+                    return true;
+                });
+
+                gantt.attachEvent('onBeforeTaskDrag', (id, mode) => {
+//                    var task = gantt.getTask(id);
+//                    var message = task.text + " ";
+//
+//                    if(mode == gantt.config.drag_mode.progress){
+//                        message += "progress is being updated";
+//                    }else{
+//                        message += "is being ";
+//                        if(mode == gantt.config.drag_mode.move)
+//                            message += "moved";
+//                        else if(mode == gantt.config.drag_mode.resize)
+//                            message += "resized";
+//                    }
+//
+//                    gantt.message(message);
+                    return true;
+                });
             }
-            return value;
         },
     },
-    data() {
-        return {
-            data: [],
-            links: [],
-        };
+    computed: {
+        ...mapGetters(['ganttData']),
+        ganttDataLinks() {
+            // {"id":"10","source":"11","target":"12","type":"1"}
+            return this
+                .ganttData
+                .filter(item => item.parent || item.milestone || item.phase)
+                .sort((a, b) => {
+                    return a.id === b.id
+                        ? 0
+                        : (a.id < b.id ? -1 : 1);
+                })
+                .map((item, key) => {
+                    return {
+                        id: key,
+                        source: item.parent || item.milestone || item.phase,
+                        target: item.id,
+                        type: 0,
+                    };
+                })
+            ;
+//            const out = [];
+//            const milestones = this
+//                .ganttData
+//                .filter(item => {
+//                    return item.type === 1;
+//                })
+//            ;
+//            const tasks = this
+//                .ganttData
+//                .filter(item => {
+//                    return item.type === 2;
+//                })
+//            ;
+//            milestones.forEach(item => {
+//                out.push({
+//                    id: out.length,
+//                    source: item.phase,
+//                    target: item.id,
+//                    type: 0,
+//                });
+//            });
+//            tasks.forEach(item => {
+//                out.push({
+//                    id: out.length,
+//                    source: item.milestone || item.phase,
+//                    target: item.id,
+//                    type: 0,
+//                });
+//            });
+//            return out;
+        },
+        ganttDataFormatted() {
+            const ganttType2WorkPackageType = [
+                gantt.config.types.milestone,
+                gantt.config.types.milestone,
+                gantt.config.types.task,
+            ];
+            return this
+                .ganttData
+                .map(item => {
+                    if (item.id == 32) {
+                        console.log(item.name);
+                        console.log(item.progress);
+                    }
+                    let out = {
+                        id: item.id,
+//                        text: item.puid + ' ' + item.name + ' / ' + item.type,
+                        text: item.name,
+                        progress: item.progress
+                            ? item.progress / 100
+                            : 0,
+                        open: true,
+                        type: ganttType2WorkPackageType[item.type],
+                    };
+
+                    let start = item.scheduledStartAt || item.forecastStartAt || item.actualStartAt;
+                    let end = item.scheduledFinishAt || item.forecastFinishAt || item.actualFinishAt;
+
+                    if (start && end) {
+                        let dtStart = new Date(start);
+                        let dtEnd = new Date(end);
+                        out.start_date = start.split('-').reverse().join('-');
+                        out.duration = (dtEnd.getTime() - dtStart.getTime()) / DAY_IN_MILISECONDS;
+                    } else if (item.createdAt && item.createdAt.match(/^(\d{4}-\d{2}-\d{2})/)) {
+                        out.start_date = item
+                            .createdAt
+                            .match(/^(\d{4}-\d{2}-\d{2})/)[0]
+                            .split('-')
+                            .reverse()
+                            .join('-')
+                        ;
+                        out.duration = 1;
+                    } else {
+                        out.unscheduled = true;
+                    }
+
+                    switch (item.type) {
+                    case 0: // phase
+                        out.parent = item.parent;
+                        break;
+                    case 1: // milestone
+                        out.parent = item.parent || item.phase;
+                        break;
+                    case 2: // task
+                        out.parent = item.parent || item.milestone || item.phase;
+                        break;
+                    }
+
+                    if (!out.parent) {
+                        delete out.parent;
+                    }
+                    return out;
+                })
+                // this has to be sorted so gantt doesn't cry
+                .sort((a, b) => {
+                    return a.id === b.id
+                        ? 0
+                        : (a.id < b.id ? -1 : 1);
+                })
+            ;
+        },
     },
 };
 </script>
 
 <style scoped="scoped">
-    @import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+@import "~dhtmlx-gantt/codebase/dhtmlxgantt.css";
+.gantt_add {
+    display: none !important;
+}
 </style>
