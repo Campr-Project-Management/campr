3ae57cff9eb4e2bec565cdf7988296c6d461a310
Vasile Codrea <vasile.codrea@trisoft.ro>
2017-07-14 14:45:34 +0300

    insert real user data into Project member page

--- frontend/src/components/Projects/Organization/ViewMember.vue
+++ frontend/src/components/Projects/Organization/ViewMember.vue
@@ -9,7 +9,7 @@
                             <i class="fa fa-angle-left"></i>
                             {{ translateText('message.back_to_organization') }}
                         </router-link>
-                        <h1>John Doe <br/><span class="small"><b class="second-color">@john.d</b></span></h1>
+                        <h1>{{member.firstName}} {{member.lastName}}<br/><span class="small"><b class="second-color">{{ member.email }}</b></span></h1>
                     </div>
                 </div>
                 <!-- /// End Header /// -->
@@ -18,7 +18,7 @@
                     <div class="col-md-6">
                         <!-- /// Member Avatar /// -->
                         <div class="avatar">
-                            <img src="http://dev.campr.biz/uploads/avatars/41.jpg" alt="John Doe">
+                            <img :src="member.gravatar" :alt="member.firstName + ' ' + member.lastName">
                         </div>
                         <!-- /// End Member Avatar /// -->
                     </div>
@@ -28,10 +28,10 @@
                         <div class="row member-details">
                             <div class="col-md-12">
                                 <div class="member-info">
-                                    <p>{{ translateText('message.role') }}: <b>Developer</b></p>
-                                    <p>{{ translateText('message.company') }}: <b>Trisoft</b></p>
-                                    <p>{{ translateText('message.department') }}: <b>Development</b></p>
-                                    <p>{{ translateText('message.subteam') }}: <b>Devs 1 Team</b></p>
+                                    <p>{{ translateText('message.role') }}: <b>{{ memberRole }}</b></p>
+                                    <p>{{ translateText('message.company') }}: <b>{{ memberCompany }}</b></p>
+                                    <p>{{ translateText('message.department') }}: <b>{{ memberDepartment }}</b></p>
+                                    <p>{{ translateText('message.subteam') }}: <b>{{ memberSubteam }}</b></p>
                                 </div>
                             </div>
                         </div>
@@ -90,19 +90,19 @@
                                 <svg version="1.1" :width="size" :height="size" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16.4">
                                     <path class="st0" d="M8,0.4c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S12.4,0.4,8,0.4z M10.4,8.1H9c0,2,0,4.7,0,4.7H7 c0,0,0-2.6,0-4.7H5.7V6.7H7V6c0-0.7,0.2-1.9,1.8-1.9l1.6,0v1.5c0,0-0.8,0-0.9,0C9.2,5.6,9,5.7,9,6.1v0.7h1.6L10.4,8.1z"/>
                                 </svg>
-                                Facebook: <a href="https://www.facebook.com/TriSoftGroup/">https://www.facebook.com/TriSoftGroup/</a>
+                                {{ translateText('label.facebook') }}: <a :href="member.facebook">{{member.facebook}}</a>
                             </div>
                             <div class="social-contanct">
                                 <svg version="1.1" :width="size" :height="size" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16.4">
                                     <path class="st0" d="M8,0.2c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S12.4,0.2,8,0.2z M11.5,6.5c0,0.1,0,0.2,0,0.2 c0,2.3-1.8,5-5,5c-1,0-1.9-0.3-2.7-0.8c0.9,0.1,1.8-0.1,2.6-0.7c-0.8,0-1.4-0.5-1.6-1.2c0.3,0,0.5,0,0.8,0C4.8,8.8,4.2,8,4.2,7.2 c0,0,0,0,0,0C4.4,7.3,4.7,7.4,5,7.4C4.5,7.1,4.2,6.5,4.2,5.9c0-0.3,0.1-0.6,0.2-0.9C5.3,6.1,6.6,6.8,8,6.9c0-0.1,0-0.3,0-0.4 c0-1,0.8-1.8,1.8-1.8c0.5,0,1,0.2,1.3,0.6c0.4-0.1,1.3-0.2,1.3-0.2C12.1,5.5,11.9,6.2,11.5,6.5z"/>
                                 </svg>
-                                Twitter: <a href="https://twitter.com/elonmusk">https://twitter.com/elonmusk</a>
+                                {{ translateText('label.twitter') }}: <a :href="member.twitter">{{ member.twitter }}</a>
                             </div>
                             <div class="social-contanct">
                                 <svg version="1.1" :width="size" :height="size" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16.4">
                                     <path class="st0" d="M8,0.4c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S12.4,0.4,8,0.4z M6.3,11.4H5V6.7h1.3V11.4z M5.7,5.4C5.3,5.4,5,5.1,5,4.7c0-0.4,0.3-0.7,0.7-0.7s0.7,0.3,0.7,0.7C6.3,5.1,6,5.4,5.7,5.4z M12.3,11.4h-2V9.1 c0-0.2-0.1-0.3-0.3-0.3c-0.2,0-0.3,0.1-0.3,0.3v2.3h-2c0,0,0-4.3,0-4.7h2v0.6c0,0,0.3-0.5,1.1-0.5c1,0,1.5,0.7,1.5,2.2V11.4z"/>
                                 </svg>
-                                Linkedin: <a href="https://www.linkedin.com/company/trisoft-group">https://www.linkedin.com/company/trisoft-group</a>
+                                {{ translateText('label.linked_in') }}: <a :href="member.linkedIn">{{ member.linkedIn }}</a>
                             </div>
                             <div class="social-contanct">
                                 <svg version="1.1" :width="size" :height="size" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16.4">
@@ -112,20 +112,20 @@
                                         <path class="st0" d="M8,0.4c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S12.4,0.4,8,0.4z M6.8,13.3 c-1.9,0-3.3-0.9-3.3-2.1c0-1.2,1.5-2.1,3.3-2.1c0.6,0,1.1,0.1,1.5,0.2C7.9,9.2,7.5,8.9,7.4,8.3c-0.2,0-0.3,0.1-0.5,0.1 c-0.6,0-1.3-0.3-1.7-0.7C4.7,7.2,4.5,6.6,4.5,6c0-0.6,0.2-1.2,0.6-1.6c0.4-0.4,1-0.6,1.6-0.6v0h2.6v0.7h-1c0,0,0.1,0,0.1,0.1 c0.4,0.4,0.7,1,0.7,1.6c0,0.6-0.2,1.2-0.6,1.6C8.4,7.9,8.2,8,8.1,8C8,8.4,8.2,8.6,8.7,8.9c0.6,0.4,1.4,0.9,1.4,2.4 C10.1,12.4,8.6,13.3,6.8,13.3z M13.3,7.1H12v1.3h-0.7V7.1H10V6.4h1.3V5.1H12v1.3h1.3V7.1z"/>
                                     </g>
                                 </svg>
-                                Google Plus: <a href="https://plus.google.com/+TrisoftRo?hl=ro">https://plus.google.com/+TrisoftRo?hl=ro</a>
+                                {{ translateText('label.gplus') }}: <a :href="member.gplus">{{ member.gplus }}</a>
                             </div>
                             <div class="social-contanct">
                                 <svg version="1.1" :width="size" :height="size" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 16 16.4">
                                     <path class="st0" d="M8,0.4c-4.4,0-8,3.6-8,8s3.6,8,8,8s8-3.6,8-8S12.4,0.4,8,0.4z M3.7,7.4l2.9,1.5l-2.9,1.7V7.4z M12.3,12.1H3.7v-0.7l3.6-2.1L8,9.6l0.8-0.4l3.6,2.1V12.1z M12.3,10.6L9.5,8.9l2.9-1.5V10.6z M12.3,5.9L8,8.1L3.7,5.9V4.7h8.7V5.9z"
                                       />
                                 </svg>
-                                Email: <a href="mailto:john.doe@campr.ro">john.doe@campr.ro</a>
+                                {{ translateText('label.email') }}: <a :href="member.email">{{ member.email }}</a>
                             </div>
                             <div class="social-contanct">
                                 <svg version="1.1" :width="size" :height="size" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 20.5 20.8">
                                 <path class="st0" d="M10.3,0.5c-5.5,0-10,4.5-10,10c0,5.5,4.5,10,10,10s10-4.5,10-10C20.3,5,15.9,0.5,10.3,0.5z M15.9,15.2l-0.7,0.7c-0.7,0.7-1.7,0.8-2.5,0.3c-3.2-2.1-6-4.9-8.1-8.1C4.1,7.3,4.2,6.3,4.9,5.6l0.7-0.7c0.5-0.5,1.4-0.5,1.9,0 l1.3,1.3c0.5,0.5,0.5,1.4,0,1.9L8.5,8.5c1.1,1.4,2.4,2.7,3.8,3.8l0.4-0.4c0.5-0.5,1.4-0.5,1.9,0l1.3,1.3 C16.5,13.8,16.5,14.7,15.9,15.2z"/>
                                 </svg>
-                                Phone: <a href="tel:+40756095992">(+4) 0756 095 992</a>
+                                {{ translateText('label.phone') }}: <a :href="member.phone">{{ member.phone }}</a>
                             </div>
                         </div>
                     </div>
@@ -145,7 +145,7 @@ export default {
         Switches,
     },
     methods: {
-        ...mapActions(['getProjectById', 'getProjectRoles', 'getProjectDepartments', 'getSubteams']),
+        ...mapActions(['getProjectById', 'getProjectRoles', 'getProjectDepartments', 'getSubteams', 'getMemberInfo']),
         translateText: function(text) {
             return this.translate(text);
         },
@@ -155,16 +155,45 @@ export default {
         this.getProjectRoles();
         this.getProjectDepartments();
         this.getSubteams();
+        if (this.$route.params.userId) {
+            this.getMemberInfo(this.$route.params.userId);
+        }
     },
-    computed: mapGetters({
-        project: 'project',
-        projectRolesForMultiSelect: 'projectRolesForMultiSelect',
-        projectDepartmentsForMultiSelect: 'projectDepartmentsForMultiSelect',
-        subteamsForSelect: 'subteamsForSelect',
-    }),
+    computed: {
+        ...mapGetters({
+            project: 'project',
+            projectRolesForMultiSelect: 'projectRolesForMultiSelect',
+            projectDepartmentsForMultiSelect: 'projectDepartmentsForMultiSelect',
+            subteamsForSelect: 'subteamsForSelect',
+            member: 'currentMember',
+        }),
+        memberCompany: function() {
+            if (this.member.projectUsers !== undefined && this.member.projectUsers.length >= 1) {
+                return this.member.projectUsers[0].company;
+            }
+            return '';
+        },
+        memberRole: function() {
+            if (this.member.projectUsers !== undefined && this.member.projectUsers.length >= 1) {
+                return this.member.projectUsers[0].projectRoleNames[0];
+            }
+            return '';
+        },
+        memberDepartment: function() {
+            if (this.member.projectUsers !== undefined && this.member.projectUsers.length >= 1) {
+                return this.member.projectUsers[0].projectDepartmentNames[0];
+            }
+            return '';
+        },
+        memberSubteam: function() {
+            if (this.member.projectUsers !== undefined && this.member.projectUsers.length >= 1) {
+                return this.member.projectUsers[0].projectTeamName;
+            }
+            return '';
+        },
+    },
     data: function() {
         return {
-            role: '',
             resource: '',
             raci: '',
             distribution: [],
