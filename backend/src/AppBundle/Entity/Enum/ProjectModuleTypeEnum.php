6125345a213282d155ed13fca41e9347165033c9
Vasile Codrea <vasile.codrea@trisoft.ro>
2017-08-21 15:46:41 +0300

    Updates to add project feature

--- backend/src/AppBundle/Entity/Enum/ProjectModuleTypeEnum.php
+++ backend/src/AppBundle/Entity/Enum/ProjectModuleTypeEnum.php
@@ -22,29 +22,84 @@ class ProjectModuleTypeEnum
     const RACI_MATRIX = 'raci_matrix';
     const TASK_CHART = 'task_chart';
     const GANTT_CHART = 'gantt_chart';
-    const CONTEXT = 'context';
     const DECISIONS = 'decisions';
 
     const ELEMENTS = [
-        self::PROJECT_CONTRACT => 'message.project_contract',
-        self::PROJECT_ORGANIZATION => 'message.project_organization',
-        self::PLAN => 'message.plan',
-        self::TASK_MANAGEMENT => 'message.task_management',
-        self::PHASES_MILESTONES => 'message.phases_milestones',
-        self::COSTS => 'message.costs',
-        self::RESOURCES => 'message.resources',
-        self::RISKS_OPPORTUNITIES => 'message.risks_opportunities',
-        self::COMMUNICATION => 'message.communication',
-        self::CONTROL_MEASURES => 'message.control_measures',
-        self::STATUS_REPORT => 'message.status_report',
-        self::MEETINGS => 'message.meetings',
-        self::TODOS => 'message.todos',
-        self::NOTES => 'message.notes',
-        self::CLOSE_DOWN_PROJECT => 'message.close_down_project',
-        self::RACI_MATRIX => 'message.raci_matrix',
-        self::TASK_CHART => 'message.task_chart',
-        self::GANTT_CHART => 'message.gantt_chart',
-        self::CONTEXT => 'message.context',
-        self::DECISIONS => 'message.decisions',
+        self::PROJECT_CONTRACT => [
+            'title' => 'modules.project_contract.title',
+            'description' => 'modules.project_contract.description',
+        ],
+        self::PROJECT_ORGANIZATION => [
+            'title' => 'modules.project_organization.title',
+            'description' => 'message.project_organization.description',
+        ],
+        self::PLAN => [
+            'title' => 'modules.plan.title',
+            'description' => 'modules.plan.description',
+        ],
+        self::TASK_MANAGEMENT => [
+            'title' => 'modules.task_management.title',
+            'description' => 'modules.task_management.description',
+        ],
+        self::PHASES_MILESTONES => [
+            'title' => 'modules.phases_milestones.title',
+            'description' => 'modules.phases_milestones.description',
+        ],
+        self::COSTS => [
+            'title' => 'modules.costs.title',
+            'description' => 'modules.costs.description',
+        ],
+        self::RESOURCES => [
+            'title' => 'modules.resources.title',
+            'description' => 'modules.resources.description',
+        ],
+        self::RISKS_OPPORTUNITIES => [
+            'title' => 'modules.risks_opportunities.title',
+            'description' => 'modules.risks_opportunities.description',
+        ],
+        self::COMMUNICATION => [
+            'title' => 'modules.communication.title',
+            'description' => 'modules.communication.description',
+        ],
+        self::CONTROL_MEASURES => [
+            'title' => 'modules.control_measures.title',
+            'description' => 'modules.control_measures.description',
+        ],
+        self::STATUS_REPORT => [
+            'title' => 'modules.status_report.title',
+            'description' => 'modules.status_report.description',
+        ],
+        self::MEETINGS => [
+            'title' => 'modules.meetings.title',
+            'description' => 'modules.meetings.description',
+        ],
+        self::TODOS => [
+            'title' => 'modules.todos.title',
+            'description' => 'modules.todos.description',
+        ],
+        self::NOTES => [
+            'title' => 'modules.notes.title',
+            'description' => 'modules.notes.description',
+        ],
+        self::CLOSE_DOWN_PROJECT => [
+           'title' => 'modules.close_down_project.title',
+            'description' => 'modules.close_down_project.description',
+        ],
+        self::RACI_MATRIX => [
+            'title' => 'modules.raci_matrix.title',
+            'description' => 'modules.raci_matrix.description',
+        ],
+        self::TASK_CHART => [
+            'title' => 'modules.task_chart.title',
+            'description' => 'modules.task_chart.description',
+        ],
+        self::GANTT_CHART => [
+            'title' => 'modules.gantt_chart.title',
+            'description' => 'modules.gantt_chart.description',
+        ],
+        self::DECISIONS => [
+            'title' => 'modules.decisions.title',
+            'description' => 'modules.decisions.description',
+        ],
     ];
 }
