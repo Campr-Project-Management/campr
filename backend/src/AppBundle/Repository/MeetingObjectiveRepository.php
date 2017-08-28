b4fc66cd0af5ab62122a40a0228234414e52c1b4
Bogdan - Andrei Nitu <bogdan.nitu@fashiondays.com>
2017-08-28 11:04:38 +0300

    #168 - sorting on meeting, todo, note, decision

--- backend/src/AppBundle/Repository/MeetingObjectiveRepository.php
+++ backend/src/AppBundle/Repository/MeetingObjectiveRepository.php
@@ -2,6 +2,9 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Repository\Traits\MeetingSortingTrait;
+
 class MeetingObjectiveRepository extends BaseRepository
 {
+    use MeetingSortingTrait;
 }
--- backend/src/AppBundle/Repository/MeetingParticipantRepository.php
+++ backend/src/AppBundle/Repository/MeetingParticipantRepository.php
@@ -2,8 +2,17 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\QueryBuilder;
+use AppBundle\Repository\Traits\MeetingSortingTrait;
+use AppBundle\Repository\Traits\UserSortingTrait;
+
 class MeetingParticipantRepository extends BaseRepository
 {
+    use MeetingSortingTrait, UserSortingTrait {
+        MeetingSortingTrait::setOrder as setMeetingOrder;
+        UserSortingTrait::setOrder as setUserOrder;
+    }
+
     public function findByWithLike(array $criteria, array $orderBy = null, $limit = null, $offset = null)
     {
         $qb = $this
@@ -23,11 +32,7 @@ class MeetingParticipantRepository extends BaseRepository
             );
         }
 
-        if ($orderBy) {
-            foreach ($orderBy as $key => $value) {
-                $qb->orderBy('m.'.$key, $value);
-            }
-        }
+        $this->setOrder($orderBy, $qb);
 
         if ($limit) {
             $qb->setMaxResults($limit);
@@ -38,5 +43,15 @@ class MeetingParticipantRepository extends BaseRepository
         }
 
         return $qb->getQuery()->getResult();
+    }
+
+    /**
+     * @param array        $orderBy
+     * @param QueryBuilder $qb
+     */
+    public function setOrder(array &$orderBy, QueryBuilder $qb)
+    {
+        $this->setMeetingOrder($orderBy, $qb);
+        $this->setUserOrder($orderBy, $qb);
     }
 }
