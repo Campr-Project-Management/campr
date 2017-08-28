45c8da113b0d5a4b68a594fefffa6955ecdc8c19
Bogdan - Andrei Nitu <bogdan.nitu@fashiondays.com>
2017-08-28 11:04:38 +0300

    #168 - sorting on meeting, todo, note, decision

--- backend/src/AppBundle/Repository/NoteRepository.php
+++ backend/src/AppBundle/Repository/NoteRepository.php
@@ -2,6 +2,27 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\QueryBuilder;
+use AppBundle\Repository\Traits\MeetingSortingTrait;
+use AppBundle\Repository\Traits\UserSortingTrait;
+use AppBundle\Repository\Traits\ProjectSortingTrait;
+
 class NoteRepository extends BaseRepository
 {
+    use MeetingSortingTrait, UserSortingTrait, ProjectSortingTrait {
+        MeetingSortingTrait::setOrder as setMeetingOrder;
+        UserSortingTrait::setOrder as setUserOrder;
+        ProjectSortingTrait::setOrder as setProjectOrder;
+    }
+
+    /**
+     * @param array        $orderBy
+     * @param QueryBuilder $qb
+     */
+    public function setOrder(array &$orderBy, QueryBuilder $qb)
+    {
+        $this->setMeetingOrder($orderBy, $qb);
+        $this->setUserOrder($orderBy, $qb);
+        $this->setProjectOrder($orderBy, $qb);
+    }
 }
