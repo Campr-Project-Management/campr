e1219bde554d0c588d5967dee173c7d6915b2193
Bogdan - Andrei Nitu <bogdan.nitu@fashiondays.com>
2017-09-07 09:04:56 +0300

    i168 - additional sorting fixes

--- backend/src/AppBundle/Repository/ProjectLimitationRepository.php
+++ backend/src/AppBundle/Repository/ProjectLimitationRepository.php
@@ -2,11 +2,16 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\QueryBuilder;
 use AppBundle\Repository\Traits\ProjectSortingTrait;
+use AppBundle\Repository\Traits\ContractSortingTrait;
 
 class ProjectLimitationRepository extends BaseRepository
 {
-    use ProjectSortingTrait;
+    use ProjectSortingTrait, ContractSortingTrait {
+        ProjectSortingTrait::setOrder as setProjectOrder;
+        ContractSortingTrait::setOrder as setContractOrder;
+    }
 
     /**
      * @param array $limitations
@@ -25,5 +30,15 @@ class ProjectLimitationRepository extends BaseRepository
                 ;
             }
         }
+    }
+
+    /**
+     * @param array        $orderBy
+     * @param QueryBuilder $qb
+     */
+    public function setOrder(array &$orderBy, QueryBuilder $qb)
+    {
+        $this->setProjectOrder($orderBy, $qb);
+        $this->setContractOrder($orderBy, $qb);
     }
 }
