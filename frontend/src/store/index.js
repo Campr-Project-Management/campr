1fa297485ec5273b8c9c6113e051de0ba6c5f9c4
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-10-11 17:36:31 -0700

    made sidebar take project.projectModules into consideration

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,65 +3,66 @@ import Vuex from 'vuex';
 import * as actions from './actions';
 import * as getters from './getters';
 import * as mutations from './mutations';
-import loader from './modules/loader';
-import user from './modules/user';
-import project from './modules/project';
-import task from './modules/task';
+import closeDownAction from './modules/close-down-action';
 import colorStatus from './modules/color_status';
-import projectStatus from './modules/project-status';
+import cost from './modules/cost';
 import customer from './modules/customer';
-import programme from './modules/programme';
+import dashboard from './modules/dashboard';
+import decisionCategory from './modules/decision-category';
+import decision from './modules/decision';
+import distributionList from './modules/distribution-list';
+import evaluationObjective from './modules/evaluation-objective';
+import gantt from './modules/gantt';
+import infoCategory from './modules/info-category';
+import info from './modules/info';
+import infoStatus from './modules/info-status';
+import lesson from './modules/lesson';
+import loader from './modules/loader';
+import measure from './modules/measure';
+import meetingAgenda from './modules/meeting-agenda';
+import meetingCategory from './modules/meeting-category';
+import meeting from './modules/meeting';
+import meetingObjective from './modules/meeting-objective';
+import meetingParticipant from './modules/meeting-participant';
+import module from './modules/module';
+import note from './modules/note';
+import noteStatus from './modules/note-status';
+import opportunity from './modules/opportunity';
+import opportunityStatus from './modules/opportunity-status';
+import opportunityStrategy from './modules/opportunity-strategy';
 import portfolio from './modules/portfolio';
-import projectContract from './modules/project-contract';
+import programme from './modules/programme';
 import projectCategory from './modules/project-category';
-import projectScope from './modules/project-scope';
-import workPackage from './modules/work-package';
+import projectCloseDown from './modules/project-close-down';
+import projectContract from './modules/project-contract';
 import projectDepartment from './modules/project-department';
-import projectUnit from './modules/project-unit';
+import project from './modules/project';
 import projectMilestone from './modules/project-milestone';
+import projectModule from './modules/project-module';
 import projectPhase from './modules/project-phase';
 import projectResources from './modules/project-resources';
-import workPackageStatus from './modules/work-package-status';
-import projectUser from './modules/project-user';
-import distributionList from './modules/distribution-list';
 import projectRole from './modules/project-role';
-import subteam from './modules/subteam';
-import opportunity from './modules/opportunity';
+import projectScope from './modules/project-scope';
+import projectStatus from './modules/project-status';
+import projectUnit from './modules/project-unit';
+import projectUser from './modules/project-user';
+import rasci from './modules/rasci';
 import risk from './modules/risk';
-import tasksStatus from './modules/tasks-status';
-import tasksByStatus from './modules/tasks-by-status';
-import opportunityStrategy from './modules/opportunity-strategy';
-import opportunityStatus from './modules/opportunity-status';
 import riskStatus from './modules/risk-status';
 import riskStrategy from './modules/risk-strategy';
-import measure from './modules/measure';
-import gantt from './modules/gantt';
-import meeting from './modules/meeting';
-import meetingCategory from './modules/meeting-category';
-import todoStatus from './modules/todo-status';
-import noteStatus from './modules/note-status';
-import meetingObjective from './modules/meeting-objective';
-import meetingAgenda from './modules/meeting-agenda';
-import decision from './modules/decision';
-import decisionCategory from './modules/decision-category';
-import todo from './modules/todo';
-import todoCategory from './modules/todo-category';
-import note from './modules/note';
-import meetingParticipant from './modules/meeting-participant';
-import validation from './modules/validation';
-import info from './modules/info';
-import infoCategory from './modules/info-category';
-import infoStatus from './modules/info-status';
-import dashboard from './modules/dashboard';
-import cost from './modules/cost';
-import module from './modules/module';
 import statusReport from './modules/status-report';
-import rasci from './modules/rasci';
-import projectCloseDown from './modules/project-close-down';
-import evaluationObjective from './modules/evaluation-objective';
-import lesson from './modules/lesson';
-import closeDownAction from './modules/close-down-action';
+import subteam from './modules/subteam';
+import task from './modules/task';
+import tasksByStatus from './modules/tasks-by-status';
+import tasksStatus from './modules/tasks-status';
+import todoCategory from './modules/todo-category';
+import todo from './modules/todo';
+import todoStatus from './modules/todo-status';
+import user from './modules/user';
+import validation from './modules/validation';
 import wbs from './modules/wbs';
+import workPackage from './modules/work-package';
+import workPackageStatus from './modules/work-package-status';
 
 Vue.use(Vuex);
 
@@ -72,65 +73,66 @@ export default new Vuex.Store({
     getters,
     mutations,
     modules: {
-        loader,
-        dashboard,
-        user,
-        distributionList,
-        project,
-        task,
+        closeDownAction,
         colorStatus,
-        projectStatus,
+        cost,
         customer,
-        programme,
-        portfolio,
-        projectContract,
-        projectCategory,
-        projectScope,
-        workPackage,
-        projectDepartment,
-        projectUser,
-        projectUnit,
-        projectMilestone,
-        projectPhase,
-        projectResources,
-        workPackageStatus,
-        projectRole,
-        subteam,
-        opportunity,
-        risk,
-        tasksStatus,
-        tasksByStatus,
-        opportunityStrategy,
-        opportunityStatus,
-        riskStatus,
-        riskStrategy,
-        measure,
-        gantt,
-        meeting,
-        meetingCategory,
-        todoStatus,
-        noteStatus,
-        meetingObjective,
-        meetingAgenda,
+        dashboard,
         decision,
         decisionCategory,
-        todo,
-        todoCategory,
-        note,
-        meetingParticipant,
-        validation,
+        distributionList,
+        evaluationObjective,
+        gantt,
         info,
         infoCategory,
         infoStatus,
-        cost,
-        module,
-        statusReport,
-        rasci,
-        projectCloseDown,
-        evaluationObjective,
         lesson,
-        closeDownAction,
+        loader,
+        measure,
+        meeting,
+        meetingAgenda,
+        meetingCategory,
+        meetingObjective,
+        meetingParticipant,
+        module,
+        note,
+        noteStatus,
+        opportunity,
+        opportunityStatus,
+        opportunityStrategy,
+        portfolio,
+        programme,
+        project,
+        projectCategory,
+        projectCloseDown,
+        projectContract,
+        projectDepartment,
+        projectMilestone,
+        projectModule,
+        projectPhase,
+        projectResources,
+        projectRole,
+        projectScope,
+        projectStatus,
+        projectUnit,
+        projectUser,
+        rasci,
+        risk,
+        riskStatus,
+        riskStrategy,
+        statusReport,
+        subteam,
+        task,
+        tasksByStatus,
+        tasksStatus,
+        todo,
+        todoCategory,
+        todoStatus,
+        user,
+        validation,
         wbs,
+        workPackage,
+        workPackageStatus,
     },
     strict: debug,
 });
