c84a045754c52449b6fe5c097f9450159420f9bc
Vasile <vasile.codrea@trisoft.ro>
2017-10-19 14:53:46 +0300

    Fixing some sorting issues in admin tool

--- backend/src/AppBundle/Repository/DistributionListRepository.php
+++ backend/src/AppBundle/Repository/DistributionListRepository.php
@@ -2,6 +2,21 @@
 
 namespace AppBundle\Repository;
 
+use Doctrine\ORM\QueryBuilder;
+use AppBundle\Repository\Traits\ProjectSortingTrait;
+
 class DistributionListRepository extends BaseRepository
 {
+    use ProjectSortingTrait {
+        ProjectSortingTrait::setOrder as setProjectOrder;
+    }
+
+    /**
+     * @param array        $orderBy
+     * @param QueryBuilder $qb
+     */
+    public function setOrder(array &$orderBy, QueryBuilder $qb)
+    {
+        $this->setProjectOrder($orderBy, $qb);
+    }
 }
