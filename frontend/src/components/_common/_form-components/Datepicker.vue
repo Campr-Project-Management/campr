655554554ad4e498bfcd6b70baef0be419a1f81d
Duma Adrian <adrian.duma@trisoft.ro>
2017-11-09 15:28:14 +0200

    datepicker extended

---
+++ frontend/src/components/_common/_form-components/Datepicker.vue
@@ -0,0 +1,15 @@
+<script>
+    import datepicker from 'vuejs-datepicker';
+    export default {
+        extends: datepicker,
+        props: ['placeholder'],
+        methods: {
+            isOpen() {
+                console.log('open datepicker');
+            },
+        },
+        mounted() {
+            console.log('datepicker mounted');
+        },
+    };
+</script>
