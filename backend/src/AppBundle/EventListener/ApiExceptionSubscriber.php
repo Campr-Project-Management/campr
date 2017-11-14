de9f67b54b063ad4359b5a5ddb1a420183974bb0
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-11-15 01:36:42 +0200

    #86

--- backend/src/AppBundle/EventListener/ApiExceptionSubscriber.php
+++ backend/src/AppBundle/EventListener/ApiExceptionSubscriber.php
@@ -2,30 +2,59 @@
 
 namespace AppBundle\EventListener;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class ApiExceptionSubscriber implements EventSubscriberInterface
 {
+    private $container;
+
     private $debug;
 
-    public function __construct($debug)
+    public function __construct(ContainerInterface $container)
     {
-        $this->debug = $debug;
+        $this->container = $container;
+        $this->debug = $container->getParameter('kernel.debug');
     }
 
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
-        if ($this->debug) {
+        $request = $event->getRequest();
+
+        if (strpos($request->getPathInfo(), '/api') !== 0) {
+            $exception = $event->getException();
+
+            if ($exception instanceof HttpException) {
+                $errorCode = $exception->getStatusCode();
+                $errorMessage = $exception->getMessage();
+            } else {
+                $errorCode = 500;
+                $errorMessage = 'Something went terribly wrong.';
+            }
+
+            $twig = $this->container->get('twig');
+            $content = $twig->render(
+                '::exception.html.twig',
+                [
+                    'code' => $errorCode,
+                    'message' => $errorMessage,
+                ]
+            );
+
+            $response = new Response($content, $errorCode);
+            $event->setResponse($response);
+
             return;
         }
 
-        $request = $event->getRequest();
-        if (strpos($request->getPathInfo(), '/api') !== 0) {
+        // on api calls we want to see the errors during development
+        if ($this->debug) {
             return;
         }
 
@@ -48,7 +77,7 @@ class ApiExceptionSubscriber implements EventSubscriberInterface
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::EXCEPTION => 'onKernelException',
+            KernelEvents::EXCEPTION => ['onKernelException', -2],
         ];
     }
 }
