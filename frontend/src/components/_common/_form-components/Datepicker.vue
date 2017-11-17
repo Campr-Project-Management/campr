67f9082b2430f15d06c1fef571a02d56eb99808c
Duma Adrian <adrian.duma@trisoft.ro>
2017-11-17 16:32:37 +0200

    identation

--- frontend/src/components/_common/_form-components/Datepicker.vue
+++ frontend/src/components/_common/_form-components/Datepicker.vue
@@ -1,73 +1,73 @@
 <template>
     <div class="datepicker" :class="wrapperClass">
         <input
-                :type="inline ? 'hidden' : 'text'"
-                :class="inputClass"
-                :name="name"
-                :id="id"
-                @click="showCalendar()"
-                :value="formattedValue"
-                :placeholder="placeholder"
-                :clear-button="clearButton"
-                readonly>
+            :type="inline ? 'hidden' : 'text'"
+            :class="inputClass"
+            :name="name"
+            :id="id"
+            @click="showCalendar()"
+            :value="formattedValue"
+            :placeholder="placeholder"
+            :clear-button="clearButton"
+            readonly>
         <i class="datepicker-clear-button" v-if="clearButton" @click="clearDate()">&times;</i>
         <!-- Day View -->
         <div class="calendar day-view" v-show="showDayView" v-bind:style="calendarStyle">
             <header>
                 <span
-                        @click="previousMonth"
-                        class="prev_custom no-select"
-                        v-bind:class="{ 'disabled' : previousMonthDisabled(currDate) }">&lt;</span>
+                    @click="previousMonth"
+                    class="prev_custom no-select"
+                    v-bind:class="{ 'disabled' : previousMonthDisabled(currDate) }">&lt;</span>
                 <span @click="showMonthCalendar" class="up">{{ currYear }} <br> {{ currMonthName }}</span>
                 <span
-                        @click="nextMonth"
-                        class="next_custom no-select"
-                        v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
+                    @click="nextMonth"
+                    class="next_custom no-select"
+                    v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
             </header>
             <span class="cell day-header" v-for="d in daysOfWeek">{{ d }}</span>
             <span class="cell day blank" v-for="d in blankDays"></span><!--
             --><span class="cell day"
-                     v-for="day in days"
-                     track-by="timestamp"
-                     v-bind:class="{ 'selected':day.isSelected, 'disabled':day.isDisabled, 'highlighted': day.isHighlighted}"
-                     @click="selectDate(day)">{{ day.date }}</span>
+                   v-for="day in days"
+                   track-by="timestamp"
+                   v-bind:class="{ 'selected':day.isSelected, 'disabled':day.isDisabled, 'highlighted': day.isHighlighted}"
+                   @click="selectDate(day)">{{ day.date }}</span>
         </div>
 
         <!-- Month View -->
         <div class="calendar" v-show="showMonthView" v-bind:style="calendarStyleSecondary">
             <header>
                 <span
-                        @click="previousYear"
-                        class="prev_custom no-select"
-                        v-bind:class="{ 'disabled' : previousYearDisabled(currDate) }">&lt;</span>
+                    @click="previousYear"
+                    class="prev_custom no-select"
+                    v-bind:class="{ 'disabled' : previousYearDisabled(currDate) }">&lt;</span>
                 <span @click="showYearCalendar" class="up">{{ getYear() }}</span>
                 <span
-                        @click="nextYear"
-                        class="next_custom no-select"
-                        v-bind:class="{ 'disabled' : nextYearDisabled(currDate) }">&gt;</span>
+                    @click="nextYear"
+                    class="next_custom no-select"
+                    v-bind:class="{ 'disabled' : nextYearDisabled(currDate) }">&gt;</span>
             </header>
             <span class="cell month"
-                  v-for="month in months"
-                  track-by="timestamp"
-                  v-bind:class="{ 'selected': month.isSelected, 'disabled': month.isDisabled }"
-                  @click.stop="selectMonth(month)">{{ month.month }}</span>
+                v-for="month in months"
+                track-by="timestamp"
+                v-bind:class="{ 'selected': month.isSelected, 'disabled': month.isDisabled }"
+                @click.stop="selectMonth(month)">{{ month.month }}</span>
         </div>
 
         <!-- Year View -->
         <div class="calendar" v-show="showYearView" v-bind:style="calendarStyleSecondary">
             <header>
                 <span @click="previousDecade" class="prev_custom no-select"
-                      v-bind:class="{ 'disabled' : previousDecadeDisabled(currDate) }">&lt;</span>
+                    v-bind:class="{ 'disabled' : previousDecadeDisabled(currDate) }">&lt;</span>
                 <span>{{ getDecade() }}</span>
                 <span @click="nextDecade" class="next_custom no-select"
-                      v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
+                    v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
             </header>
             <span
-                    class="cell year"
-                    v-for="year in years"
-                    track-by="timestamp"
-                    v-bind:class="{ 'selected': year.isSelected, 'disabled': year.isDisabled }"
-                    @click.stop="selectYear(year)">{{ year.year }}</span>
+                class="cell year"
+                v-for="year in years"
+                track-by="timestamp"
+                v-bind:class="{ 'selected': year.isSelected, 'disabled': year.isDisabled }"
+                @click.stop="selectYear(year)">{{ year.year }}</span>
         </div>
 
     </div>
@@ -150,13 +150,13 @@
                        background: transparent;
                     }
                     &.next_custom{
-                         width: 14.28%;
-                         float: right;
-                         font-size: 44px;
-                         position: relative;
-                         font-weight: 100;
-                         padding-right: 22px;
-                         cursor: pointer;
+                        width: 14.28%;
+                        float: right;
+                        font-size: 44px;
+                        position: relative;
+                        font-weight: 100;
+                        padding-right: 22px;
+                        cursor: pointer;
                     }
                     &.prev_custom{
                         width: 14.28%;
