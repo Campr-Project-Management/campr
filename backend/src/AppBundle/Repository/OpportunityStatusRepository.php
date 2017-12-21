9c6e9e1229052189fe6815a51e752ddb744347ce
Radu Gribincea <radu.gribincea@trisoft.ro>
2017-12-21 06:00:28 +0200

    #281

--- backend/src/AppBundle/Repository/OpportunityStatusRepository.php
+++ backend/src/AppBundle/Repository/OpportunityStatusRepository.php
@@ -2,9 +2,28 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Project;
 use AppBundle\Repository\Traits\ProjectSortingTrait;
 
 class OpportunityStatusRepository extends BaseRepository
 {
     use ProjectSortingTrait;
+
+    public function findAllByProjectNullable(Project $project)
+    {
+        $qb = $this->createQueryBuilder('os');
+
+        $qb
+            ->where(
+                $qb
+                    ->expr()
+                    ->orX(
+                        $qb->expr()->isNull('os.project'),
+                        $qb->expr()->eq('os.project', $project->getId())
+                    )
+            )
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
 }
