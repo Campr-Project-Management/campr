3812b06cf067b77a7b7f5c50791ae2040a965215
Radu Gribincea <radu.gribincea@trisoft.ro>
2018-01-11 08:53:57 +0200

    added translation selection

---
+++ backend/src/MainBundle/EventListener/LocaleListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace MainBundle\EventListener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+
+class LocaleListener
+{
+    private $defaultLocale;
+
+    public function __construct($defaultLocale = 'en')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        $request = $event->getRequest();
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
+
+        if ($locale = $request->attributes->get('_locale')) {
+            $request->getSession()->set('_locale', $locale);
+        } else {
+            $request->setLocale($request->getSession()->get('_locale', $this->defaultLocale));
+        }
+    }
+}
