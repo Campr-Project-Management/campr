155ddbd04e148e0f33b90f3e3875055742cb3714
Radu Gribincea <radu.gribincea@trisoft.ro>
2018-01-26 19:50:46 +0200

    fix for setting team lead

--- backend/src/AppBundle/Form/Subteam/MemberType.php
+++ backend/src/AppBundle/Form/Subteam/MemberType.php
@@ -7,13 +7,10 @@ use AppBundle\Entity\SubteamRole;
 use AppBundle\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\CallbackTransformer;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Validator\Constraints\Range;
 
-class SubteamMemberType extends AbstractType
+class MemberType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -25,14 +22,7 @@ class SubteamMemberType extends AbstractType
                     'class' => 'selectpicker',
                 ],
             ])
-            ->add('isLead', IntegerType::class, [
-                'constraints' => [
-                    new Range([
-                        'min' => 0,
-                        'max' => 1,
-                    ]),
-                ],
-            ])
+            ->add('isLead')
             ->add('subteamRoles', EntityType::class, [
                 'class' => SubteamRole::class,
                 'required' => true,
@@ -41,20 +31,6 @@ class SubteamMemberType extends AbstractType
                     'class' => 'selectpicker',
                 ],
             ])
-        ;
-
-        $builder
-            ->get('isLead')
-            ->addModelTransformer(
-                new CallbackTransformer(
-                    function ($val) {
-                        return intval($val);
-                    },
-                    function ($val) {
-                        return intval($val);
-                    }
-                )
-            )
         ;
     }
 
