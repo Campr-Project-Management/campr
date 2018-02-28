9e0d9a3469fe3dd58c872b956dd3a3c569e8c938
Radu Gribincea <radu.gribincea@trisoft.ro>
2018-02-28 12:09:06 +0200

    addded ssr deploy

--- deploy.php
+++ deploy.php
@@ -7,7 +7,14 @@ require 'recipe/symfony3.php';
 // Set configurations
 set('repository', 'git@lab.trisoft.ro:campr/campr.git');
 set('shared_files', []);
-set('shared_dirs', ['var/logs', 'backend/vendor', 'web/uploads', 'frontend/node_modules', 'var/profiler']);
+set('shared_dirs', [
+    'var/logs',
+    'backend/vendor',
+    'web/uploads',
+    'frontend/node_modules',
+    'ssr/node_modules',
+    'var/profiler',
+]);
 set('writable_dirs', ['var/cache', '../../shared/var/logs', '../../shared/web/uploads', '../../shared/var/profiler']);
 set('http_user', 'www-data');
 
@@ -227,6 +234,9 @@ task('server:provision', function () {
 task('project:build:frontend', function () {
     run('cd {{release_path}}/frontend && npm install && npm run build');
 });
+task('project:build:ssr', function () {
+    run('cd {{release_path}}/ssr && npm install && npm run build');
+});
 task('hivebot:deploy-whois', function () {
     set('localUser', sprintf(
         '%s',
@@ -279,6 +289,7 @@ task('deploy', [
     'deploy:symlink',
     'project:front-static',
     'project:build:frontend',
+    'project:build:ssr',
     'deploy:cache:warmup',
     'database:cleanup',
     'database:migrate',
