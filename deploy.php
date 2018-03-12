07d39817a2d00b671e0160ac03e2cf899a9cc2f3
Radu Topala <radu.topala@trisoft.ro>
2018-03-12 15:19:35 +0000

    moved to yarn

--- deploy.php
+++ deploy.php
@@ -11,6 +11,8 @@ set('shared_dirs', [
     'var/logs',
     'backend/vendor',
     'web/uploads',
+    'frontend/node_modules',
+    'ssr/node_modules',
     'var/profiler',
 ]);
 set('writable_dirs', ['var/cache', '../../shared/var/logs', '../../shared/web/uploads', '../../shared/var/profiler']);
@@ -230,10 +232,10 @@ task('server:provision', function () {
     }
 });
 task('project:build:frontend', function () {
-    run('cd {{release_path}}/frontend && npm install && npm run build');
+    run('cd {{release_path}}/frontend && yarn install && yarn run build');
 });
 task('project:build:ssr', function () {
-    run('cd {{release_path}}/ssr && npm install && npm run build');
+    run('cd {{release_path}}/ssr && yarn install && yarn run build');
 });
 task('hivebot:deploy-whois', function () {
     set('localUser', sprintf(
