7b6a60de69810db55d8b1d9addbdc32cff2ed3a5
Viorel Craescu <viorel@craescu.com>
2018-03-16 15:56:02 +0200

    issue_437

--- backend/src/AppBundle/AppBundle.php
+++ backend/src/AppBundle/AppBundle.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle;
 
+use AppBundle\DependencyInjection\CompilerPass\RegisterRepositoryPass;
 use AppBundle\DependencyInjection\ContainerBuilder\DynamicDatabaseConnectionPass;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
@@ -9,10 +10,14 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class AppBundle extends Bundle
 {
+    /**
+     * @param ContainerBuilder $container
+     */
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
 
-        $container->addCompilerPass(new DynamicDatabaseConnectionPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, -256);
+        $container->addCompilerPass(new DynamicDatabaseConnectionPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION);
+        $container->addCompilerPass(new RegisterRepositoryPass());
     }
 }
