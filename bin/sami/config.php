ad2f2780993a2c25db723c51d736233760ff1a09
Radu Topala <radu.topala@trisoft.ro>
2018-04-10 22:20:46 +0300

    sami update

--- bin/sami/config.php
+++ bin/sami/config.php
@@ -6,12 +6,12 @@ use Symfony\Component\Finder\Finder;
 $iterator = Finder::create()
     ->files()
     ->exclude('Tests')
-    ->in(__DIR__.'/../../src')
+    ->in(__DIR__.'/../../backend/src')
 ;
 
 return new Sami($iterator, array(
     'title'                => 'Campr Project Documentation',
-    'build_dir'            => __DIR__.'/../../src/AppBundle/Resources/docs/build/',
-    'cache_dir'            => __DIR__.'/../../src/AppBundle/Resources/docs/cache/',
+    'build_dir'            => __DIR__.'/../../backend/src/AppBundle/Resources/docs/build/',
+    'cache_dir'            => __DIR__.'/../../backend/src/AppBundle/Resources/docs/cache/',
     'default_opened_level' => 2,
 ));
