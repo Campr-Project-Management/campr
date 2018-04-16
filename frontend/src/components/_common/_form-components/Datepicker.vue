efaabc093635bb9221a913ddb1ac0e7431274836
Viorel Craescu <viorel@craescu.com>
2018-04-16 18:00:36 +0300

    issue_452

--- frontend/src/components/_common/_form-components/Datepicker.vue
+++ frontend/src/components/_common/_form-components/Datepicker.vue
@@ -1,75 +1,73 @@
 <template>
     <div class="datepicker" :class="wrapperClass">
         <input
-            :type="inline ? 'hidden' : 'text'"
-            :class="inputClass"
-            :name="name"
-            :id="id"
-            @click="onClick()"
-            :value="formattedValue"
-            :placeholder="placeholder"
-            :clear-button="clearButton"
-            readonly>
+                :type="inline ? 'hidden' : 'text'"
+                :class="inputClass"
+                :name="name"
+                :id="id"
+                @click="onClick()"
+                :value="formattedValue"
+                :placeholder="placeholder"
+                :clear-button="clearButton"
+                readonly>
         <i class="datepicker-clear-button" v-if="clearButton" @click="clearDate()">&times;</i>
-        <!-- Day View -->
         <div class="calendar day-view" v-show="showDayView" v-bind:style="calendarStyle">
             <header>
                 <span
-                    @click="previousMonth"
-                    class="prev_custom no-select"
-                    v-bind:class="{ 'disabled' : previousMonthDisabled(currDate) }">&lt;</span>
+                        @click="previousMonth"
+                        class="prev_custom no-select"
+                        v-bind:class="{ 'disabled' : previousMonthDisabled(currDate) }">&lt;</span>
                 <span @click="showMonthCalendar" class="up">{{ currYear }} <br> {{ currMonthName }}</span>
                 <span
-                    @click="nextMonth"
-                    class="next_custom no-select"
-                    v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
+                        @click="nextMonth"
+                        class="next_custom no-select"
+                        v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
             </header>
             <span class="cell day-header" v-for="d in daysOfWeek">{{ d }}</span>
-            <span class="cell day blank" v-for="d in blankDays"></span><!--
-            --><span class="cell day"
-                   v-for="day in days"
-                   track-by="timestamp"
-                   v-bind:class="{ 'selected':day.isSelected, 'disabled':day.isDisabled, 'highlighted': day.isHighlighted}"
-                   @click="selectDate(day)">{{ day.date }}</span>
+            <span class="cell day blank" v-for="d in blankDays"></span>
+            <span class="cell day"
+                  v-for="day in days"
+                  track-by="timestamp"
+                  v-bind:class="{ 'selected':day.isSelected, 'disabled':day.isDisabled, 'highlighted': day.isHighlighted}"
+                  @click="selectDate(day)">{{ day.date }}</span>
         </div>
 
         <!-- Month View -->
         <div class="calendar" v-show="showMonthView" v-bind:style="calendarStyleSecondary">
             <header>
                 <span
-                    @click="previousYear"
-                    class="prev_custom no-select"
-                    v-bind:class="{ 'disabled' : previousYearDisabled(currDate) }">&lt;</span>
+                        @click="previousYear"
+                        class="prev_custom no-select"
+                        v-bind:class="{ 'disabled' : previousYearDisabled(currDate) }">&lt;</span>
                 <span @click="showYearCalendar" class="up">{{ getYear() }}</span>
                 <span
-                    @click="nextYear"
-                    class="next_custom no-select"
-                    v-bind:class="{ 'disabled' : nextYearDisabled(currDate) }">&gt;</span>
+                        @click="nextYear"
+                        class="next_custom no-select"
+                        v-bind:class="{ 'disabled' : nextYearDisabled(currDate) }">&gt;</span>
             </header>
             <span class="cell month"
-                v-for="month in months"
-                track-by="timestamp"
-                v-bind:class="{ 'selected': month.isSelected, 'disabled': month.isDisabled }"
-                @click.stop="selectMonth(month)">{{ month.month }}</span>
+                  v-for="month in months"
+                  track-by="timestamp"
+                  v-bind:class="{ 'selected': month.isSelected, 'disabled': month.isDisabled }"
+                  @click.stop="selectMonth(month)">{{ month.month }}</span>
         </div>
 
         <!-- Year View -->
         <div class="calendar" v-show="showYearView" v-bind:style="calendarStyleSecondary">
             <header>
                 <span @click="previousDecade" class="prev_custom no-select"
-                    v-bind:class="{ 'disabled' : previousDecadeDisabled(currDate) }">&lt;</span>
+                      v-bind:class="{ 'disabled' : previousDecadeDisabled(currDate) }">&lt;</span>
                 <span>{{ getDecade() }}</span>
                 <span @click="nextDecade" class="next_custom no-select"
-                    v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
+                      v-bind:class="{ 'disabled' : nextMonthDisabled(currDate) }">&gt;</span>
             </header>
             <span
-                class="cell year"
-                v-for="year in years"
-                track-by="timestamp"
-                v-bind:class="{ 'selected': year.isSelected, 'disabled': year.isDisabled }"
-                @click.stop="selectYear(year)">{{ year.year }}</span>
+                    class="cell year"
+                    v-for="year in years"
+                    track-by="timestamp"
+                    v-bind:class="{ 'selected': year.isSelected, 'disabled': year.isDisabled }"
+                    @click.stop="selectYear(year)">{{ year.year }}</span>
         </div>
-
     </div>
 </template>
 
@@ -78,6 +76,7 @@
 
     export default {
         extends: datepicker,
+        name: 'datepicker',
         props: {
             placeholder: {
                 type: String,
@@ -90,21 +89,6 @@
             },
         },
         methods: {
-            isOpen() {
-                let scrollTop = $(window).scrollTop();
-                let elementOffset = $(this.$el).offset().top;
-                let currentElementOffset = (elementOffset - scrollTop);
-
-                let windowInnerHeight = window.innerHeight;
-
-                if (windowInnerHeight - currentElementOffset < 350) {
-                    $(this.$el).find('.calendar').addClass('bottom-up');
-                } else {
-                    $(this.$el).find('.calendar').removeClass('bottom-up');
-                }
-
-                return this.showDayView || this.showMonthView || this.showYearView;
-            },
             onClick() {
                 if (this.disabledPicker) {
                     return;
@@ -117,60 +101,60 @@
 </script>
 
 <style scoped lang="scss">
-    .datepicker{
-        input{
+    .datepicker {
+        input {
             color: #8794C4;
             text-transform: uppercase;
             font-size: 11px;
             text-align: left;
             &::-webkit-input-placeholder { /* WebKit, Blink, Edge */
-                color:    #8794C4;
+                color: #8794C4;
             }
             &::-moz-placeholder { /* Mozilla Firefox 4 to 18 */
-                color:    #8794C4;
-                opacity:  1;
+                color: #8794C4;
+                opacity: 1;
             }
             &::-moz-placeholder { /* Mozilla Firefox 19+ */
-                color:    #8794C4;
-                opacity:  1;
+                color: #8794C4;
+                opacity: 1;
             }
             &::-ms-input-placeholder { /* Internet Explorer 10-11 */
-                color:    #8794C4;
+                color: #8794C4;
             }
             &::-ms-input-placeholder { /* Microsoft Edge */
-                color:    #8794C4;
+                color: #8794C4;
             }
         }
-        .calendar{
+        .calendar {
             top: 40px;
             background: white;
             padding: 15px;
             width: 277px;
-            &.bottom-up{
+            &.bottom-up {
                 top: -280px;
             }
-            &.day-view{
-                header{
-                    .up{
+            &.day-view {
+                header {
+                    .up {
                         line-height: 20px;
                         text-align: center;
                     }
                 }
             }
-            header{
+            header {
                 overflow: hidden;
                 background: #009F7E;
                 margin: -15px;
                 margin-bottom: 10px;
-                >span{
+                > span {
                     display: block;
                     padding: 34px 0px;
                     font-size: 16px;
                     font-weight: 600;
-                    &:hover{
-                       background: transparent;
+                    &:hover {
+                        background: transparent;
                     }
-                    &.next_custom{
+                    &.next_custom {
                         width: 14.28%;
                         float: right;
                         font-size: 44px;
@@ -179,7 +163,7 @@
                         padding-right: 22px;
                         cursor: pointer;
                     }
-                    &.prev_custom{
+                    &.prev_custom {
                         width: 14.28%;
                         float: left;
                         font-size: 44px;
@@ -188,26 +172,26 @@
                         padding-left: 22px;
                         cursor: pointer;
                     }
-                 }
+                }
             }
-            .cell{
+            .cell {
                 color: #282114;
                 border-radius: 4px;
                 height: 24px;
                 width: 35px;;
                 line-height: 25px;
-                &.day-header{
+                &.day-header {
                     font-weight: 600;
                     width: 35px;
                 }
-                &.month, &.year{
+                &.month, &.year {
                     width: 33.333%;
                 }
-                &.selected{
+                &.selected {
                     background: #FF0055;
                     color: white;
                 }
-                &:hover{
+                &:hover {
                     border: 0px !important;
                     background: #E8E8E8;
                 }
