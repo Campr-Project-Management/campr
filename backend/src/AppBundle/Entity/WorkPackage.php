00acf02eb15fd1aadbd349cfdb15eb384f308099
Viorel Craescu <viorel@craescu.com>
2018-04-19 12:30:03 +0300

    issue_470

--- backend/src/AppBundle/Entity/WorkPackage.php
+++ backend/src/AppBundle/Entity/WorkPackage.php
@@ -8,11 +8,13 @@ use JMS\Serializer\Annotation as Serializer;
 use Gedmo\Mapping\Annotation as Gedmo;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 use Symfony\Component\Validator\Constraints as Assert;
+use AppBundle\Validator\Constraints\WorkPackageAssignments;
 
 /**
  * WorkPackage.
  *
  * @ORM\Entity(repositoryClass="AppBundle\Repository\WorkPackageRepository")
+ * @WorkPackageAssignments()
  */
 class WorkPackage
 {
