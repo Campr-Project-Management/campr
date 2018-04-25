2cdd8be982f51c4865eebc1f8b96d59c0ef1148b
Viorel Craescu <viorel@craescu.com>
2018-04-25 14:20:14 +0300

    issue_395

---
+++ backend/src/Component/Project/ProjectEvents.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Component\WorkPackage;
+
+final class ProjectEvents
+{
+    public const PRE_CREATE = 'app.project.pre_update';
+    public const POST_CREATE = 'app.project.post_update';
+    public const PRE_UPDATE = 'app.project.pre_update';
+    public const POST_UPDATE = 'app.project.post_update';
+}
