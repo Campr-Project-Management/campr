b628534fc03daddd1f85909b64f2286d877ccddd
Radu Topala <radu.topala@trisoft.ro>
2018-04-27 12:11:14 +0000

    #293 - mkdir

--- backend/src/AppBundle/Command/FileSystemRefreshCommand.php
+++ backend/src/AppBundle/Command/FileSystemRefreshCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use AppBundle\Entity\FileSystem as TeamFileSystem;
+use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Refreshes all or one local file-system.
@@ -28,11 +29,19 @@ class FileSystemRefreshCommand extends ContainerAwareCommand
 
         $rootPath = $this->getContainer()->getParameter('uploads.root_folder');
 
+        $fs = new Filesystem();
+
         $fileSystems = $input->getOption('all') ? $em->getRepository(TeamFileSystem::class)->findBy(['driver' => TeamFileSystem::LOCAL_ADAPTER]) : ($input->hasOption('name') ? $em->getRepository(TeamFileSystem::class)->findBy(['name' => $input->getOption('name'), 'driver' => TeamFileSystem::LOCAL_ADAPTER]) : []);
 
         foreach ($fileSystems as $fileSystem) {
+            $teamFolder = sprintf('%s/%s', $rootPath, $fileSystem->getName());
+
+            if (!$fs->exists($teamFolder)) {
+                $fs->mkdir($teamFolder);
+            }
+
             $fileSystem->setConfig([
-                'path' => sprintf('%s/%s', $rootPath, $fileSystem->getName()),
+                'path' => $teamFolder,
             ]);
 
             $em->persist($fileSystem);
