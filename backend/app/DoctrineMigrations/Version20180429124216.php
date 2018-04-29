5d99aae80a9272bb84d5ab2b01723c846601c288
georgiana.paiu <georgiana.paiu@trisoft.ro>
2018-04-29 09:43:37 +0000

    fixed database schema

--- backend/app/DoctrineMigrations/Version20180429124216.php
+++ backend/app/DoctrineMigrations/Version20180429124216.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180429091335 extends AbstractMigration
+class Version20180429124216 extends AbstractMigration
 {
     /**
      * @param Schema $schema
