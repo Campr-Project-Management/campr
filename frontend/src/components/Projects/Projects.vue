3c01fcc1d49fa44ecb8797df6122d14f2d09e09c
georgiana.paiu <georgiana.paiu@trisoft.ro>
2018-04-30 11:30:39 +0000

    fixed pagination - active page

--- frontend/src/components/Projects/Projects.vue
+++ frontend/src/components/Projects/Projects.vue
@@ -19,6 +19,7 @@
         <pagination
             :current-page="activePage"
             :number-of-pages="pages"
+            :value="activePage"
             v-on:change-page="changePage"/>
     </div>
 </template>
