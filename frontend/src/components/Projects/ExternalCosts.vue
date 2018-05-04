4b710178b33e827c0c8bf431e512bec46a154364
Viorel Craescu <viorel@craescu.com>
2018-05-04 11:27:13 +0300

    issue_336

--- frontend/src/components/Projects/ExternalCosts.vue
+++ frontend/src/components/Projects/ExternalCosts.vue
@@ -2,29 +2,33 @@
     <div class="page-section">
         <div class="header flex flex-space-between">
             <div class="flex">
-                <h1>{{ translateText('message.external_costs') }}</h1>
+                <h1>{{ translate('message.external_costs') }}</h1>
             </div>
         </div>
-        <chart title="message.by_phase" :data="externalCostsGraphData.byPhase"/>
+        <chart
+                title="message.by_phase"
+                :data="externalCostsGraphData.byPhase | chartData"/>
         <hr class="double">
-        <chart title="message.by_department" :data="externalCostsGraphData.byDepartment"/>
+
+        <chart
+                title="message.by_department"
+                :data="externalCostsGraphData.byDepartment | chartData"/>
     </div>
 </template>
 
 <script>
     import Chart from './Charts/CostsChart.vue';
+    import filters from './Charts/mixins/filters';
     import {mapGetters, mapActions} from 'vuex';
 
     export default {
         name: 'project-external-costs',
+        mixins: [filters],
         components: {
             Chart,
         },
         methods: {
             ...mapActions(['getProjectExternalCostsGraphData']),
-            translateText: function(text) {
-                return this.translate(text);
-            },
         },
         created() {
             this.getProjectExternalCostsGraphData({id: this.$route.params.id});
@@ -36,10 +40,3 @@
         },
     };
 </script>
-
-<style scoped lang="scss">
-    @import '../../css/_common';
-    @import '../../css/_mixins';
-    @import '../../css/_variables';
-    @import '../../css/page-section';
-</style>
