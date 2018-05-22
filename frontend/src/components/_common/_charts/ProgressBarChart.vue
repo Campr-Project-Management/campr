9903286888553e4ee58b75ca21989da8803452cf
Viorel Craescu <viorel@craescu.com>
2018-05-22 08:53:23 +0300

    issue_572

--- frontend/src/components/_common/_charts/ProgressBarChart.vue
+++ frontend/src/components/_common/_charts/ProgressBarChart.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="progress-bar-chart clearfix flex">
+    <div class="progress-bar-chart">
         <div
                 v-for="(data, index) in series"
                 :key="index"
                 :style="{width: getPercent(data) + '%', backgroundColor: data.color}"
-                class="bar flex-v-center status-bar"
+                class="bar"
                 v-tooltip.top-center="translate(data.name)">
 
             <span v-if="options.labels.enabled">{{ translate(data.name) }}: </span>
@@ -72,6 +72,8 @@
     .progress-bar-chart {
         width: 100%;
         margin: 1em 0;
+        max-width: 100%;
+        display: table;
 
         .bar {
             height: 30px;
@@ -83,7 +85,7 @@
             text-transform: uppercase;
             letter-spacing: 0.1em;
             min-width: 7em;
-            display: inline-table;
+            display: table-cell;
         }
     }
 </style>
