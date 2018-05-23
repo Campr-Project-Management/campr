f20fe38f22477387019f0e848cba7834010510dc
Viorel Craescu <viorel@craescu.com>
2018-05-23 16:26:01 +0300

    issue_593

--- backend/src/Component/WorkPackage/Calculator/PhaseStatusCalculator.php
+++ backend/src/Component/WorkPackage/Calculator/PhaseStatusCalculator.php
@@ -10,15 +10,52 @@ use Webmozart\Assert\Assert;
 
 class PhaseStatusCalculator implements StatusCalculatorInterface
 {
+    private static $rules = [
+        [
+            WorkPackageStatus::CODE_ONGOING,
+            [
+                WorkPackageStatus::CODE_PENDING,
+                WorkPackageStatus::CODE_COMPLETED,
+                WorkPackageStatus::CODE_CLOSED,
+                WorkPackageStatus::CODE_OPEN,
+            ],
+            WorkPackageStatus::CODE_ONGOING,
+        ],
+        [
+            WorkPackageStatus::CODE_PENDING,
+            [
+                WorkPackageStatus::CODE_OPEN,
+            ],
+            WorkPackageStatus::CODE_PENDING,
+        ],
+        [
+            WorkPackageStatus::CODE_PENDING,
+            [
+                WorkPackageStatus::CODE_OPEN,
+                WorkPackageStatus::CODE_ONGOING,
+                WorkPackageStatus::CODE_COMPLETED,
+                WorkPackageStatus::CODE_CLOSED,
+            ],
+            WorkPackageStatus::CODE_ONGOING,
+        ],
+        [
+            WorkPackageStatus::CODE_COMPLETED,
+            [
+                WorkPackageStatus::CODE_CLOSED,
+            ],
+            WorkPackageStatus::CODE_COMPLETED,
+        ],
+    ];
+
     /**
      * @var WorkPackage
      */
-    private $workPackageRepository;
+    protected $workPackageRepository;
 
     /**
      * @var WorkPackageStatusRepository
      */
-    private $workPackageStatusRepository;
+    protected $workPackageStatusRepository;
 
     /**
      * @param WorkPackageRepository       $workPackageRepository
@@ -39,12 +76,119 @@ class PhaseStatusCalculator implements StatusCalculatorInterface
      */
     public function calculate(WorkPackage $workPackage): WorkPackageStatus
     {
-        Assert::true($workPackage->isPhase(), 'WorkPackage is not a phase');
+        Assert::true($this->isSupported($workPackage), 'WorkPackage is not a phase');
 
-        $status = $workPackage->getWorkPackageStatus();
+        $status = $this->calculateStatus($workPackage);
         if (!$status) {
             $status = $this->workPackageStatusRepository->getDefault();
         }
+
+        return $status;
+    }
+
+    /**
+     * @param WorkPackage $workPackage
+     *
+     * @return bool
+     */
+    protected function isSupported(WorkPackage $workPackage)
+    {
+        return $workPackage->isPhase();
+    }
+
+    /**
+     * @param WorkPackage $workPackage
+     *
+     * @return array
+     */
+    protected function getStatusesCodes(WorkPackage $workPackage)
+    {
+        $codes = [];
+        foreach ($this->getStatuses() as $status) {
+            $count = $this->workPackageRepository->getStatusCountByPhase($workPackage, $status);
+            if (!$count) {
+                continue;
+            }
+
+            $codes[] = $status->getCode();
+        }
+
+        return $codes;
+    }
+
+    /**
+     * @param WorkPackage $workPackage
+     *
+     * @return WorkPackageStatus|null
+     */
+    private function calculateStatus(WorkPackage $workPackage)
+    {
+        $codes = $this->getStatusesCodes($workPackage);
+        if (empty($codes)) {
+            return null;
+        }
+
+        $code = $this->calculateStatusCode($codes);
+
+        return $this->findStatusByCode($code);
+    }
+
+    /**
+     * @param array $codes
+     *
+     * @return string
+     */
+    private function calculateStatusCode(array $codes): string
+    {
+        $codes = array_unique($codes);
+        Assert::notEmpty($codes);
+
+        if (1 === count($codes)) {
+            return array_shift($codes);
+        }
+
+        foreach (self::$rules as $rule) {
+            if (!in_array($rule[0], $codes)) {
+                continue;
+            }
+
+            $diff = array_diff(
+                array_filter(
+                    $codes,
+                    function (string $code) use ($rule) {
+                        return $code !== $rule[0];
+                    }
+                ),
+                $rule[1]
+            );
+
+            if (!empty($diff)) {
+                continue;
+            }
+
+            return $rule[2];
+        }
+
+        return WorkPackageStatus::CODE_OPEN;
+    }
+
+    /**
+     * @return WorkPackageStatus[]
+     */
+    protected function getStatuses(): array
+    {
+        return $this->workPackageStatusRepository->findAll();
+    }
+
+    /**
+     * @param string $code
+     *
+     * @return WorkPackageStatus
+     */
+    private function findStatusByCode(string $code): WorkPackageStatus
+    {
+        /** @var WorkPackageStatus $status */
+        $status = $this->workPackageStatusRepository->findOneBy(['code' => $code]);
 
         return $status;
     }
