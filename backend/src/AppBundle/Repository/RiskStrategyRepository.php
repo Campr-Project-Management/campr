21368231163c2d15c473f5b1cba2f2d1ccb58b20
georgiana.paiu <georgiana.paiu@trisoft.ro>
2018-05-28 14:36:21 +0000

    get directly all strategies using findAll

--- backend/src/AppBundle/Repository/RiskStrategyRepository.php
+++ backend/src/AppBundle/Repository/RiskStrategyRepository.php
@@ -2,28 +2,9 @@
 
 namespace AppBundle\Repository;
 
-use AppBundle\Entity\Project;
 use AppBundle\Repository\Traits\ProjectSortingTrait;
 
 class RiskStrategyRepository extends BaseRepository
 {
     use ProjectSortingTrait;
-
-    public function findAllByProjectNullable(Project $project)
-    {
-        $qb = $this->createQueryBuilder('rs');
-
-        $qb
-            ->where(
-                $qb
-                    ->expr()
-                    ->orX(
-                        $qb->expr()->isNull('rs.project'),
-                        $qb->expr()->eq('rs.project', $project->getId())
-                    )
-            )
-        ;
-
-        return $qb->getQuery()->getResult();
-    }
 }
