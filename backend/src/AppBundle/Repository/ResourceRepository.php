7aa88ddcd932ff6b13974e79b8eb45717d299aab
georgiana.paiu <georgiana.paiu@trisoft.ro>
2018-06-04 21:25:01 +0000

    #405vvvvvv

--- backend/src/AppBundle/Repository/ResourceRepository.php
+++ backend/src/AppBundle/Repository/ResourceRepository.php
@@ -2,9 +2,38 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Project;
 use AppBundle\Repository\Traits\ProjectSortingTrait;
 
 class ResourceRepository extends BaseRepository
 {
     use ProjectSortingTrait;
+
+    /**
+     * @param Project $project
+     *
+     * @return resource|null
+     */
+    public function findWithoutProjectUserOrWithShowInResourcesProjectUserByProject(Project $project)
+    {
+        $qb = $this->createQueryBuilder('r');
+        $qb->leftJoin('r.projectUser', 'pu');
+        $qb->where(
+            $qb->expr()->eq(
+                'r.project',
+                $project->getId()
+            )
+        );
+        $qb->andWhere(
+            $qb
+                ->expr()
+                ->orX(
+                    $qb->expr()->isNull('r.projectUser'),
+                    $qb->expr()->eq('pu.showInResources',  true)
+                )
+            )
+        ;
+
+        return $qb->getQuery()->getResult();
+    }
 }
