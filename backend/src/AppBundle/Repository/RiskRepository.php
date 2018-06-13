b2c08659d43642a733cbafa372ddbf589b51bfd0
Viorel Craescu <viorel@craescu.com>
2018-06-13 15:09:11 +0300

    issue_637

--- backend/src/AppBundle/Repository/RiskRepository.php
+++ backend/src/AppBundle/Repository/RiskRepository.php
@@ -17,11 +17,6 @@ class RiskRepository extends BaseRepository
         UserSortingTrait::setOrder as setUserOrder;
     }
 
-    const VERY_LOW = [0, 25];
-    const LOW = [25, 50];
-    const HIGH = [50, 75];
-    const VERY_HIGH = [75, 100];
-
     public function getQueryBuilderByProject(Project $project)
     {
         return $this
@@ -47,98 +42,6 @@ class RiskRepository extends BaseRepository
         ;
     }
 
-    public function findFiltered(Project $project, $filters = [])
-    {
-        $qb = $this->getQueryBuilderByProject($project);
-
-        if (isset($filters['probability'])) {
-            switch ($filters['probability']) {
-                case 1:
-                    $qb->andWhere('r.probability >= '.self::VERY_LOW[0]);
-                    $qb->andWhere('r.probability <= '.self::VERY_LOW[1]);
-                    break;
-                case 2:
-                    $qb->andWhere('r.probability > '.self::LOW[0]);
-                    $qb->andWhere('r.probability <= '.self::LOW[1]);
-                    break;
-                case 3:
-                    $qb->andWhere('r.probability > '.self::HIGH[0]);
-                    $qb->andWhere('r.probability <= '.self::HIGH[1]);
-                    break;
-                case 4:
-                    $qb->andWhere('r.probability > '.self::VERY_HIGH[0]);
-                    $qb->andWhere('r.probability <= '.self::VERY_HIGH[1]);
-                    break;
-            }
-        }
-        if (isset($filters['impact'])) {
-            switch ($filters['impact']) {
-                case 1:
-                    $qb->andWhere('r.impact >= '.self::VERY_LOW[0]);
-                    $qb->andWhere('r.impact <= '.self::VERY_LOW[1]);
-                    break;
-                case 2:
-                    $qb->andWhere('r.impact > '.self::LOW[0]);
-                    $qb->andWhere('r.impact <= '.self::LOW[1]);
-                    break;
-                case 3:
-                    $qb->andWhere('r.impact > '.self::HIGH[0]);
-                    $qb->andWhere('r.impact <= '.self::HIGH[1]);
-                    break;
-                case 4:
-                    $qb->andWhere('r.impact > '.self::VERY_HIGH[0]);
-                    $qb->andWhere('r.impact <= '.self::VERY_HIGH[1]);
-                    break;
-            }
-        }
-
-        return $qb->getQuery()->getResult();
-    }
-
-    public function getGridCount($projectId)
-    {
-        $sql = '
-            SELECT
-                CASE
-                    WHEN probability <= :very_low_1 THEN 1
-                    WHEN probability > :low_0 AND probability <= :low_1 THEN 2
-                    WHEN probability > :high_0 AND probability <= :high_1 THEN 3
-                    WHEN probability > :very_high_0 THEN 4
-                    ELSE 1
-                END as probability,
-                CASE
-                    WHEN impact <= :very_low_1 THEN 1
-                    WHEN impact > :low_0 AND impact <= :low_1 THEN 2
-                    WHEN impact > :high_0 AND impact <= :high_1 THEN 3
-                    WHEN impact > :very_high_0 THEN 4
-                    ELSE 1
-                END as impact
-            FROM `risk`
-            WHERE project_id = :project_id
-        ';
-
-        $stmt = $this->getEntityManager()->getConnection()->prepare($sql);
-        $stmt->execute([
-            ':very_low_1' => self::VERY_LOW[1],
-            ':low_0' => self::LOW[0],
-            ':low_1' => self::LOW[1],
-            ':high_0' => self::HIGH[0],
-            ':high_1' => self::HIGH[1],
-            ':very_high_0' => self::VERY_HIGH[0],
-            ':project_id' => $projectId,
-        ]);
-        $result = $stmt->fetchAll();
-        $data = [];
-        foreach ($result as $res) {
-            $data[$res['probability'].'-'.$res['impact']] = array_key_exists($res['probability'].'-'.$res['impact'], $data)
-                ? $data[$res['probability'].'-'.$res['impact']] + 1
-                : 1
-            ;
-        }
-
-        return $data;
-    }
-
     /**
      * @param Project $project
      *
@@ -146,7 +49,6 @@ class RiskRepository extends BaseRepository
      */
     public function getStatsByProject(Project $project)
     {
-        $gridValues = $this->getGridCount($project->getId());
         $averageData = $this->getAverageData($project);
         $costs = $this->getTotalPotentialCosts($project);
         $delay = $this->getTotalPotentialDelay($project);
@@ -155,7 +57,6 @@ class RiskRepository extends BaseRepository
             'costs' => $costs,
             'delay' => round($delay, 2),
             'averageData' => $averageData,
-            'gridValues' => $gridValues,
         ];
     }
 
