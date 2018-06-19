6186d8d2203697f20c0433cb25705b420bafa03d
Viorel Craescu <viorel@craescu.com>
2018-06-19 13:07:47 +0300

    issue_558

--- frontend/src/components/_common/_phases-and-milestones-components/MilestoneFilters.vue
+++ frontend/src/components/_common/_phases-and-milestones-components/MilestoneFilters.vue
@@ -4,116 +4,117 @@
         <div class="dropdowns">
             <div class="flex flex-space-between dates">
                 <div class="input-holder right">
-                    <datepicker :placeholder="translateText('label.due_date')" @cleared="clearDueDate()" v-bind:clear-button="true" v-model="dueDate" format="dd - MM - yyyy" :value="dueDate"></datepicker>
-                    <calendar-icon fill="middle-fill"></calendar-icon>
+                    <date-field
+                            :placeholder="translateText('label.due_date')"
+                            @cleared="clearDueDate()"
+                            :clear-button="true"
+                            v-model="dueDate"/>
                 </div>
             </div>
             <select-field
-                v-if="projectPhases.items && projectPhases.items.length"
-                v-bind:title="'Phase'"
-                v-bind:options="projectPhasesForSelect"
-                v-model="phaseModel"
-                v-bind:currentOption="phaseModel" />
+                    v-if="projectPhases.items && projectPhases.items.length"
+                    v-bind:title="'Phase'"
+                    v-bind:options="projectPhasesForSelect"
+                    v-model="phaseModel"
+                    v-bind:currentOption="phaseModel"/>
             <select-field
-                v-if="!boardView"
-                v-bind:title="translateText('message.status')"
-                v-bind:options="workPackageStatusesForMilestone"
-                v-model="statusModel"
-                v-bind:currentOption="statusModel" />
+                    v-if="!boardView"
+                    v-bind:title="translateText('message.status')"
+                    v-bind:options="workPackageStatusesForMilestone"
+                    v-model="statusModel"
+                    v-bind:currentOption="statusModel"/>
             <select-field
-                v-bind:title="translateText('label.responsible')"
-                v-bind:options="projectUsersForSelect"
-                v-model="responsibleModel"
-                v-bind:currentOption="responsibleModel" />
+                    v-bind:title="translateText('label.responsible')"
+                    v-bind:options="projectUsersForSelect"
+                    v-model="responsibleModel"
+                    v-bind:currentOption="responsibleModel"/>
             <a @click="clearFilters()" class="btn-rounded btn-auto second-bg">{{ translateText('button.clear_filters') }}</a>
         </div>
     </div>
 </template>
 
 <script>
-import Dropdown from '../Dropdown2';
-import {mapActions, mapGetters} from 'vuex';
-import datepicker from '../_form-components/Datepicker';
-import CalendarIcon from '../../_common/_icons/CalendarIcon';
-import SelectField from '../../_common/_form-components/SelectField';
+    import Dropdown from '../Dropdown2';
+    import {mapActions, mapGetters} from 'vuex';
+    import SelectField from '../../_common/_form-components/SelectField';
+    import DateField from '../_form-components/DateField';
 
-export default {
-    props: ['clearAllFilters', 'selectStatus', 'selectResponsible', 'selectPhase', 'selectDueDate'],
-    created() {
-        this.getWorkPackageStatuses();
-        this.getProjectUsers({id: this.$route.params.id});
-        if (!this.projectPhases.items || this.projectPhases.items.length === 0) {
-            this.getProjectPhases({projectId: this.$route.params.id});
-        }
-    },
-    components: {
-        Dropdown,
-        datepicker,
-        CalendarIcon,
-        SelectField,
-    },
-    computed: {
-        ...mapGetters({
-            workPackageStatusesForMilestone: 'workPackageStatusesForMilestone',
-            projectUsersForSelect: 'projectUsersForSelect',
-            projectPhases: 'projectPhases',
-            projectPhasesForSelect: 'projectPhasesForSelect',
-        }),
-    },
-    methods: {
-        ...mapActions(['getWorkPackageStatuses', 'getProjectUsers', 'getProjectPhases']),
-        translateText: function(text) {
-            return this.translate(text);
-        },
-        clearDueDate: function() {
-            this.dueDate = null;
-        },
-        clearFilters: function() {
-            this.clearDueDate();
-            this.$refs.phases.resetCustomTitle();
-            this.statusModel = null;
-            this.phaseModel = null;
-            this.responsibleModel = null;
-            this.$refs.responsibles.resetCustomTitle();
-            this.clearAllFilters(true);
-        },
-    },
-    data() {
-        return {
-            dueDate: '',
-            statusModel: null,
-            responsibleModel: null,
-            phaseModel: null,
-        };
-    },
-    watch: {
-        dueDate: function(value) {
-            this.dueDate = value;
-            this.selectDueDate(value);
-        },
-        statusModel: function(value) {
-            if (this.statusModel !== null) {
-                this.selectStatus(value.key);
-            } else {
-                this.selectStatus(null);
+    export default {
+        props: ['clearAllFilters', 'selectStatus', 'selectResponsible', 'selectPhase', 'selectDueDate'],
+        created() {
+            this.getWorkPackageStatuses();
+            this.getProjectUsers({id: this.$route.params.id});
+            if (!this.projectPhases.items || this.projectPhases.items.length === 0) {
+                this.getProjectPhases({projectId: this.$route.params.id});
             }
         },
-        responsibleModel: function(value) {
-            if (this.responsibleModel !== null) {
-                this.selectResponsible(value.key);
-            } else {
-                this.selectResponsible(null);
-            }
+        components: {
+            DateField,
+            Dropdown,
+            SelectField,
         },
-        phaseModel: function(value) {
-            if (this.phaseModel !== null) {
-                this.selectPhase(value.key);
-            } else {
-                this.selectPhase(null);
-            }
+        computed: {
+            ...mapGetters({
+                workPackageStatusesForMilestone: 'workPackageStatusesForMilestone',
+                projectUsersForSelect: 'projectUsersForSelect',
+                projectPhases: 'projectPhases',
+                projectPhasesForSelect: 'projectPhasesForSelect',
+            }),
         },
-    },
-};
+        methods: {
+            ...mapActions(['getWorkPackageStatuses', 'getProjectUsers', 'getProjectPhases']),
+            translateText: function(text) {
+                return this.translate(text);
+            },
+            clearDueDate: function() {
+                this.dueDate = null;
+            },
+            clearFilters: function() {
+                this.clearDueDate();
+                this.$refs.phases.resetCustomTitle();
+                this.statusModel = null;
+                this.phaseModel = null;
+                this.responsibleModel = null;
+                this.$refs.responsibles.resetCustomTitle();
+                this.clearAllFilters(true);
+            },
+        },
+        data() {
+            return {
+                dueDate: '',
+                statusModel: null,
+                responsibleModel: null,
+                phaseModel: null,
+            };
+        },
+        watch: {
+            dueDate: function(value) {
+                this.dueDate = value;
+                this.selectDueDate(value);
+            },
+            statusModel: function(value) {
+                if (this.statusModel !== null) {
+                    this.selectStatus(value.key);
+                } else {
+                    this.selectStatus(null);
+                }
+            },
+            responsibleModel: function(value) {
+                if (this.responsibleModel !== null) {
+                    this.selectResponsible(value.key);
+                } else {
+                    this.selectResponsible(null);
+                }
+            },
+            phaseModel: function(value) {
+                if (this.phaseModel !== null) {
+                    this.selectPhase(value.key);
+                } else {
+                    this.selectPhase(null);
+                }
+            },
+        },
+    };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
