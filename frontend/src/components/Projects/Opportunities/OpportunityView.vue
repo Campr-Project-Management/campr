e8cc5179bed4809d571a233f3465fb4e1a3c55f0
Radu Gribincea <radu.gribincea@trisoft.ro>
2018-06-19 21:39:03 +0000

    #337 + migration fix + composer/docker fix

--- frontend/src/components/Projects/Opportunities/OpportunityView.vue
+++ frontend/src/components/Projects/Opportunities/OpportunityView.vue
@@ -86,8 +86,8 @@
                     <div class="col-md-12">
                         <div class="ro-info">
                             <p>{{ translate('message.priority') }}: <b v-if="priority" :class="priority.color">{{ translate(priority.name) }}</b><b v-else>-</b></p>
-                            <p>{{ translate('message.strategy') }}: <b>{{ opportunity.opportunityStrategyName }}</b></p>
-                            <p>{{ translate('message.status') }}: <b>{{ opportunity.opportunityStatusName }}</b></p>
+                            <p>{{ translate('message.strategy') }}: <b v-if="opportunityStrategyName">{{ translate(opportunity.opportunityStrategyName) }}</b><b v-else>-</b></b></p>
+                            <p>{{ translate('message.status') }}: <b>{{ translate(opportunity.opportunityStatusName) }}</b></p>
                         </div>
 
                         <div class="ro-info">
