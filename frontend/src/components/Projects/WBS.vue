461f6a5b093c93b91ad7a6ac45a04cb6c256ed7d
Viorel Craescu <viorel@craescu.com>
2018-07-05 15:37:12 +0000

    issue_685

--- frontend/src/components/Projects/WBS.vue
+++ frontend/src/components/Projects/WBS.vue
@@ -158,7 +158,7 @@ export default {
                 .appendEntityWireframe()
                 .appendEntityLabel(this.updateTree)
                 .appendEntityTopMenu()
-                .appendTrafficLightLabel()
+                .appendStatus()
                 .appendProgressLabel()
                 .appendPUIDLabel()
                 .appendDates()
