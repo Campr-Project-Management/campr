322f1b19df5130e8360fe177160da1314ce7a9e4
georgiana.paiu <georgiana.paiu@trisoft.ro>
2018-07-15 09:05:43 +0000

    implemented a scroll bar on risk or opportunity preview list

--- frontend/src/components/Projects/ProjectRisksOpportunities.vue
+++ frontend/src/components/Projects/ProjectRisksOpportunities.vue
@@ -15,15 +15,15 @@
 
                 <!-- /// Project Opportunities /// -->
                 <div class="ro-grid-wrapper clearfix">
-                    <div class="row">
-                        <div class="col-md-8">
+                    <div class="row equal">
+                        <div class="col-md-8" ref="opportunityMatrix">
                             <opportunity-matrix
                                     :clickable="true"
                                     :show-summary="false"
                                     @priorityClick="onOpportunityPriorityClick"
                                     :labels="opportunitiesMatrixLabels" />
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-4" v-bind:style="opportunityRightColStyle">
                             <opportunity-list :list="selectedOpportunities"/>
                         </div>
                     </div>
@@ -50,14 +50,14 @@
 
                 <div class="ro-grid-wrapper clearfix">
                     <div class="row">
-                        <div class="col-md-8">
+                        <div class="col-md-8" ref="riskMatrix">
                             <risk-matrix
                                     :clickable="true"
                                     :show-summary="false"
                                     :labels="risksMatrixLabels"
                                     @priorityClick="onRiskPriorityClick"/>
                         </div>
-                        <div class="col-md-4">
+                        <div class="col-md-4"  v-bind:style="RiskRightColStyle">
                             <risk-list :list="selectedRisks"/>
                         </div>
                     </div>
@@ -197,6 +197,11 @@
                     return value.impactIndex === impactIndex && value.probabilityIndex === probabilityIndex;
                 });
             },
+            matchHeight() {
+                console.log(this.$refs);
+                this.opportunityRightColStyle.height = this.$refs.opportunityMatrix.clientHeight + 'px';
+                this.riskRightColStyle.height = this.$refs.riskMatrix.clientHeight + 'px';
+            },
         },
         created() {
             this.getProjectOpportunities({projectId: this.$route.params.id});
@@ -207,7 +212,16 @@
             return {
                 selectedRiskPriority: null,
                 selectedOpportunityPriority: null,
+                opportunityRightColStyle: {
+                    height: 'auto',
+                },
+                riskRightColStyle: {
+                    height: 'auto',
+                },
             };
+        },
+        mounted() {
+            this.matchHeight();
         },
     };
 </script>
@@ -217,6 +231,11 @@
     @import '../../css/page-section';
     @import '../../css/_mixins';
     @import '../../css/_variables';
+
+    .row.equal {
+        display: flex;
+        flex-wrap: wrap;
+    }
 
     .dark-border-right {
         border-right: 1px solid $darkerColor;
