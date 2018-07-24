4de6010374e80967e4f2cc71669819df76958f06
Viorel Craescu <viorel@craescu.com>
2018-07-24 05:58:48 +0000

    issue_682

---
+++ backend/src/Component/Calculator/DaysElapsedProgressCalculator.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Component\Calculator;
+
+use Component\Date\DateRange;
+use Webmozart\Assert\Assert;
+
+class DaysElapsedProgressCalculator implements DateRangeCalculatorInterface
+{
+    /**
+     * @param \DateTime $start
+     * @param \DateTime $end
+     *
+     * @return float
+     */
+    public function calculate(\DateTime $start, \DateTime $end): float
+    {
+        $start = clone $start;
+        $start->setTime(0, 0, 0);
+
+        $end = clone $end;
+        $end->setTime(0, 0, 0);
+
+        Assert::lessThanEq($start, $end, sprintf('Start date must be less than or equal to end date'));
+
+        $today = $this->getReferenceDate();
+        if ($today >= $end) {
+            return 100;
+        }
+
+        $dateRange = new DateRange($start, $end);
+        $total = $dateRange->getDurationDays();
+
+        $todayRange = new DateRange($start, $today);
+        $days = $todayRange->getDurationDays();
+
+        return round(($days / $total) * 100, 4);
+    }
+
+    /**
+     * @return \DateTime
+     */
+    protected function getReferenceDate(): \DateTime
+    {
+        $today = new \DateTime('2019-09-30');
+        $today->setTime(0, 0, 0);
+
+        return $today;
+    }
+}
