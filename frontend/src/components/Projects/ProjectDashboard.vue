8f1bafeeaca5a766c4d44cba0b73f2ee99ed7e6d
Alin Florin <alin.lefter@trisoft.ro>
2018-08-14 16:05:53 +0300

    issue #460

--- frontend/src/components/Projects/ProjectDashboard.vue
+++ frontend/src/components/Projects/ProjectDashboard.vue
@@ -159,8 +159,8 @@
                                     v-bind:task="task"/>
                         </div>
                         <div class="margintop20 buttons">
-                            <router-link :to="{name: 'project-task-management-list'}" class="btn-rounded btn-md btn-empty btn-auto">{{ translate('button.view_all_tasks') }}</router-link>
                             <router-link :to="{name: 'project-task-management-list'}" class="btn-rounded btn-md btn-empty btn-auto">{{ translate('button.view_tasks_board') }}</router-link>
+                            <router-link :to="{name: 'project-task-management-list-grid'}" class="btn-rounded btn-md btn-empty btn-auto">{{ translate('button.view_tasks_grid') }}</router-link>
                         </div>
                     </div>
                 </div>
