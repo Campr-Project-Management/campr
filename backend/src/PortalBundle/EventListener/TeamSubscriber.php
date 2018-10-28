a3528bd043ef72adc797c17fcf508af9f5bf469d
ioana <ioana.hazsda@trisoft.ro>
2018-10-28 19:34:13 +0200

    campr#717 Beta workspace.

--- backend/src/PortalBundle/EventListener/TeamSubscriber.php
+++ backend/src/PortalBundle/EventListener/TeamSubscriber.php
@@ -3,6 +3,7 @@
 namespace PortalBundle\EventListener;
 
 use AppBundle\Command\RedisQueueManagerCommand;
+use AppBundle\Model\BetaTeam;
 use AppBundle\Entity\Team;
 use Component\Team\TeamEvents;
 use Predis\Client;
@@ -28,17 +29,24 @@ class TeamSubscriber implements EventSubscriberInterface
     private $logger;
 
     /**
+     * @var string
+     */
+    private $betaDbPath;
+
+    /**
      * TeamSubscriber constructor.
      *
      * @param                 $env
      * @param Client          $redis
      * @param LoggerInterface $logger
+     * @param string          $betaDbPath
      */
-    public function __construct($env, Client $redis, LoggerInterface $logger)
+    public function __construct($env, Client $redis, LoggerInterface $logger, $betaDbPath)
     {
         $this->env = $env;
         $this->redis = $redis;
         $this->logger = $logger;
+        $this->betaDbPath = $betaDbPath;
     }
 
     /**
@@ -48,6 +56,7 @@ class TeamSubscriber implements EventSubscriberInterface
     {
         return [
             TeamEvents::POST_CREATE => 'onTeamPostCreate',
+            TeamEvents::BETA_POST_CREATE => 'onBetaTeamPostCreate',
             TeamEvents::POST_RESTORE => 'onTeamPostRestore',
         ];
     }
@@ -59,6 +68,8 @@ class TeamSubscriber implements EventSubscriberInterface
     {
         /** @var Team $team */
         $team = $event->getSubject();
+        $this->logger->info('user', $team->getUser());
+
         if (!($team instanceof Team)) {
             return;
         }
@@ -130,6 +141,89 @@ class TeamSubscriber implements EventSubscriberInterface
         );
 
         $this->logger->info('Commands to create new team successfully sent', ['slug' => $team->getSlug()]);
+    }
+
+    /**
+     * @param GenericEvent $event
+     */
+    public function onBetaTeamPostCreate(GenericEvent $event)
+    {
+        /** @var BetaTeam $betaTeam */
+        $betaTeam = $event->getSubject();
+        $team = $betaTeam->getTeam();
+
+        if (!($team instanceof Team)) {
+            return;
+        }
+
+        $teamEnv = sprintf('%s_%s', str_replace('-', '_', $team->getSlug()), $this->env);
+
+        $this->logger->info('Sending commands to create new beta team', ['slug' => $team->getSlug()]);
+
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf('--env=%s doctrine:database:create -n', $teamEnv),
+            ]
+        );
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf('--env=%s doctrine:database:import %s -n', $teamEnv, $this->betaDbPath),
+            ]
+        );
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf(
+                    '--env=%s app:setup-beta %s',
+                    $teamEnv,
+                    $betaTeam->getEmail()
+                ),
+            ]
+        );
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf(
+                    '--env=%s app:file-system:create %s',
+                    $teamEnv,
+                    $team->getSlug()
+                ),
+            ]
+        );
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf(
+                    '--env=%s fos:js-routing:dump --target=web/static/js/fos_js_routes_%s.js',
+                    $teamEnv,
+                    $team->getSlug()
+                ),
+            ]
+        );
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf(
+                    '--env=%s app:unlock-team %s',
+                    $this->env,
+                    $team->getSlug()
+                ),
+            ]
+        );
+        $this->redis->rpush(
+            RedisQueueManagerCommand::DEFAULT,
+            [
+                sprintf(
+                    '--env=%s portal:webhook:workspace:created %s',
+                    $this->env,
+                    $team->getUuid()
+                ),
+            ]
+        );
+
+        $this->logger->info('Commands to create new beta team successfully sent', ['slug' => $team->getSlug()]);
     }
 
     /**
