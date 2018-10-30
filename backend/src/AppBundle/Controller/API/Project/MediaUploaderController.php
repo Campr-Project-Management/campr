2c1219b295237e4126c4b529527eed342b4cecaa
Viorel Craescu <viorel@craescu.com>
2018-10-30 11:23:49 +0000

    fix

--- backend/src/AppBundle/Controller/API/Project/MediaUploaderController.php
+++ backend/src/AppBundle/Controller/API/Project/MediaUploaderController.php
@@ -3,9 +3,13 @@
 namespace AppBundle\Controller\API\Project;
 
 use Oneup\UploaderBundle\Controller\DropzoneController;
+use Oneup\UploaderBundle\Uploader\ErrorHandler\ErrorHandlerInterface;
 use Oneup\UploaderBundle\Uploader\Exception\ValidationException;
 use Oneup\UploaderBundle\Uploader\Response\EmptyResponse;
+use Oneup\UploaderBundle\Uploader\Storage\StorageInterface;
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -15,11 +19,39 @@ use Symfony\Component\HttpFoundation\Response;
 class MediaUploaderController extends DropzoneController
 {
     /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * MediaUploaderController constructor.
+     *
+     * @param ContainerInterface    $container
+     * @param StorageInterface      $storage
+     * @param ErrorHandlerInterface $errorHandler
+     * @param array                 $config
+     * @param                       $type
+     * @param LoggerInterface       $logger
+     */
+    public function __construct(
+        ContainerInterface $container,
+        StorageInterface $storage,
+        ErrorHandlerInterface $errorHandler,
+        array $config,
+        $type,
+        LoggerInterface $logger
+    ) {
+        parent::__construct($container, $storage, $errorHandler, $config, $type);
+
+        $this->logger = $logger;
+    }
+
+    /**
      * @Route("/upload", name="app_api_project_uploader_media_upload", options={"expose"=true}, methods={"POST", "PUT"})
      *
      * @return JsonResponse
      */
-    public function upload()
+    public function uploadAction(): JsonResponse
     {
         $request = $this->getRequest();
         $response = new EmptyResponse();
@@ -45,14 +77,16 @@ class MediaUploaderController extends DropzoneController
 
                 return $response;
             } catch (\Exception $e) {
+                $this->logger->error($e->getMessage(), ['trace' => $e->getTraceAsString()]);
+
                 $translator = $this->container->get('translator');
-                $message = $translator->trans($e->getMessage());
+                $message = $translator->trans('message.generic_error');
                 $response = $this->createSupportedJsonResponse(
                     [
                         'messages' => [$message],
                     ]
                 );
-                $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+                $response->setStatusCode(Response::HTTP_BAD_REQUEST);
 
                 return $response;
             }
