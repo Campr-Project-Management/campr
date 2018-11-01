051af774490a0d714aa60c4680a9bfab49c016b4
Radu Gribincea <radu.gribincea@trisoft.ro>
2018-11-01 10:41:54 +0200

    #723 - A1

--- ssr/components/Charts/CostsChart.vue
+++ ssr/components/Charts/CostsChart.vue
@@ -5,6 +5,36 @@
     export default {
         name: 'charts-costs-chart',
         extends: CostsChart,
+        mounted() {
+            let limit = 80;
+            let x = setInterval(() => {
+                if (!limit--) {
+                    clearInterval(x);
+                    return;
+                }
+
+                let elem = this.$el;
+
+                while (true) {
+                    if (
+                        elem.style
+                        && elem.style.position === 'absolute'
+                        && elem.attributes
+                        && elem.attributes.getNamedItem('aria-label')
+                        && elem.attributes.getNamedItem('aria-label').value === 'A chart.'
+                        && elem.attributes.removeNamedItem('style')
+                    ) {
+                        break;
+                    }
+                    if (!elem || !elem.children || !elem.children.length) {
+                        return;
+                    }
+                    elem = elem.children[0];
+                }
+
+                clearInterval(x);
+            }, 25);
+        },
         data() {
             return {
                 defaultOptions: {
