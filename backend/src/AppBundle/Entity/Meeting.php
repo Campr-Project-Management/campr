3136908d93fd71a03d9299073216507ea972e054
Ioana Hazsda <ioana.hazsda@trisoft.ro>
2018-11-12 07:07:09 +0000

    Fix.

--- backend/src/AppBundle/Entity/Meeting.php
+++ backend/src/AppBundle/Entity/Meeting.php
@@ -808,22 +808,6 @@ class Meeting
         return $this->meetingObjectives;
     }
 
-    public function getMeetingAgendasTotalDuration()
-    {
-        $totalDuration = 0;
-
-        foreach ($this->getMeetingAgendas() as $agenda) {
-            $totalDuration += $agenda->getDuration();
-        }
-
-        return $totalDuration;
-    }
-
-    public function getMeetingDuration()
-    {
-        return ($this->getEnd()->getTimestamp() - $this->getStart()->getTimestamp()) / 60;
-    }
-
     /**
      * @return int
      */
