47fa91d6abda9ac47acb0ebcf75b17e2f2c7e550
Viorel Craescu <viorel@craescu.com>
2019-02-11 08:48:13 +0000

    issue_764

--- backend/src/Component/WorkPackage/WorkPackageEvents.php
+++ backend/src/Component/WorkPackage/WorkPackageEvents.php
@@ -4,8 +4,8 @@ namespace Component\WorkPackage;
 
 final class WorkPackageEvents
 {
-    public const PRE_CREATE = 'app.workpackage.pre_update';
-    public const POST_CREATE = 'app.workpackage.post_update';
+    public const PRE_CREATE = 'app.workpackage.pre_create';
+    public const POST_CREATE = 'app.workpackage.post_create';
     public const PRE_UPDATE = 'app.workpackage.pre_update';
     public const POST_UPDATE = 'app.workpackage.post_update';
 }
