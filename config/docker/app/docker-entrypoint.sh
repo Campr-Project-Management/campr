93f3c7b238b67e6a0cbfe6cb7d1ff595464e5843
Radu Gribincea <radu.gribincea@trisoft.ro>
2020-04-16 13:41:03 +0300

    on-premise build fix

--- config/docker/app/docker-entrypoint.sh
+++ config/docker/app/docker-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 
 if [ ! -d '/app/backend/vendor' ] || [ ! -f "/app/backend/vendor/autoload.php" ]; then
-  cd /app && composer install --no-scripts
-  cd /app/ssr && yarn
-  cd /app/frontend && yarn && yarn build
+  runuser -l $APPLICATION_USER -c 'cd /app && composer install --no-scripts'
+  runuser -l $APPLICATION_USER -c 'cd /app/ssr && yarn'
+  runuser -l $APPLICATION_USER -c 'cd /app/frontend && yarn && yarn build'
 fi
 
 if [ "$1" = 'supervisord' ] || [ "$1" = 'bin/console' ]; then
-	mkdir -p var/cache var/log public/media web/static/js /dev/shm/campr/cache/
-	setfacl -R -m u:www-data:rwX -m u:application:rwX -m g:application:rwX -m u:"$(whoami)":rwX var web/static web/uploads /dev/shm/campr/
-	setfacl -dR -m u:www-data:rwX -m u:application:rwX -m g:application:rwX -m u:"$(whoami)":rwX var web/static web/uploads /dev/shm/campr/
+  mkdir -p var/cache var/log public/media web/static/js /dev/shm/campr/cache/
+  setfacl -R -m u:www-data:rwX -m u:application:rwX -m g:application:rwX -m u:"$(whoami)":rwX var web/static web/uploads /dev/shm/campr
+  setfacl -dR -m u:www-data:rwX -m u:application:rwX -m g:application:rwX -m u:"$(whoami)":rwX var web/static web/uploads /dev/shm/campr
 
   # MacOS // Vagrant
   if [[ "${VAGRANT}" == "VAGRANT" ]]; then
@@ -17,22 +17,28 @@ if [ "$1" = 'supervisord' ] || [ "$1" = 'bin/console' ]; then
     chmod -R 777 /dev/shm/campr
   fi
 
-  composer install
+  runuser -l $APPLICATION_USER -c 'cd /app && composer install'
 
   if [ -f "/app/bin/console" ]; then
-    bin/console doc:database:create --if-not-exists
+    runuser -l $APPLICATION_USER -c 'cd /app && bin/console doc:database:create --if-not-exists'
 
-    until bin/console doctrine:query:sql "select 1" >/dev/null 2>&1; do
+    until runuser -l $APPLICATION_USER -c 'cd /app && bin/console doctrine:query:sql "select 1" >/dev/null 2>&1'; do
       (>&2 echo "Waiting for MySQL to be ready...")
       sleep 1
     done
 
-    bin/console doc:database:create --if-not-exists
+    runuser -l $APPLICATION_USER -c 'cd /app && bin/console doc:database:create --if-not-exists'
 
-    bin/console app:migrate:all-databases
+    runuser -l $APPLICATION_USER -c 'cd /app && bin/console app:migrate:all-databases'
   fi
 
-  cd /app/frontend && yarn && cd /app
+#  runuser -l $APPLICATION_USER -c 'cd /app/frontend && yarn && yarn build'
+
+  chown -R "$APPLICATION_USER:$APPLICATION_GROUP" /app
+
+  runuser -l $APPLICATION_USER -c 'cd /app && bin/console cache:clear'
+  runuser -l $APPLICATION_USER -c 'cd /app && bin/front-static'
+  runuser -l $APPLICATION_USER -c 'cd /app/frontend && yarn build'
 fi
 
 exec "$@"
