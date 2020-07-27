d344a5614eb197a7c685c3cb068a6c1f7171c589
Maxim Gavrilenko <maxim@impltech.com>
2020-07-27 17:26:32 +0300

    modified new task email

--- ssr/plugins/translator/en.js
+++ ssr/plugins/translator/en.js
@@ -2647,7 +2647,10 @@ export default function (Translator) {
     Translator.add("two_factor.remove.email_success", "A link has been sent to your email to remove the two factor authentication.", "messages", "en");
     Translator.add("email.subject.two_factor.remove", "CAMPR - Two Factor authentication removal", "messages", "en");
     Translator.add("email.two_factor.body.remove", "Click <a href=\"%url%\">here<\/a> to remove two factor authentication from your account.", "messages", "en");
-    Translator.add("email.message.task.new", "Hello %task_responsible%, %task_creator% created a new task \"%task_name%\" with due date %base_finished_date% for you. Workspace: %workspace_name% Project: %project_name% Taskname: %task_name%", "messages", "en");
+    Translator.add("email.message.task.new", "Hello %task_responsible%, %task_creator% created a new task \"%task_name%\" with due date %base_finished_date% for you.", "messages", "en");
+    Translator.add("email.message.task.new.workspace", 'Workspace: %workspace_name%', "messages", "en");
+    Translator.add("email.message.task.new.project", 'Project: %project_name%', "messages", "en");
+    Translator.add("email.message.task.new.taskcreator", 'Creator of the task: %task_creator%', "messages", "en");
     Translator.add("email.button.task.profile", "Task Profile", "messages", "en");
     Translator.add("email.subject.task.new", "Your new task: \"%workspace_name%\" - \"%project_name%\" - \"%task_name%\"", "messages", "en");
     Translator.add("email.subject.task.edit", "Your task responsibility has changed \"%workspace_name%\" - \"%project_name%\" - \"%task_name%\"", "messages", "en");
