bbfde15536364a16d005a5c3381cdae1e2fdd6bc
Maxim Gavrilenko <maxim@impltech.com>
2020-08-03 09:45:18 +0300

    #50 updated redirects after login pages

--- backend/src/PortalBundle/Controller/SecurityController.php
+++ backend/src/PortalBundle/Controller/SecurityController.php
@@ -10,6 +10,7 @@ use AppBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Webmozart\Assert\Assert;
 
