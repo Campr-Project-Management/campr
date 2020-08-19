503009f2cd3c79c48054f5dfc40d76308949464c
Maxim Gavrilenko <maxim@impltech.com>
2020-08-19 14:17:22 +0300

    updated migtation project_status

--- backend/app/DoctrineMigrations/Version20200811110857.php
+++ backend/app/DoctrineMigrations/Version20200811110857.php
@@ -22,15 +22,24 @@ class Version20200811110857 extends AbstractMigration
             'Migration can only be executed safely on \'mysql\'.'
         );
 
-        $this->addSql('INSERT INTO `' . $this->table. '`
-         (`id`, `name`, `sequence`, `created_at`, `updated_at`, `code`)
-            VALUES 
-            (1,\'label.not_started\',0,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.not_started\'),
-            (2,\'label.in_progress\',1,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.in_progress\'),
-            (3,\'label.pending\',2,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.pending\'),
-            (4,\'label.open\',3,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.open\'),
-            (5,\'label.closed\',4,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.closed\')
-        ');
+        $stmt = $this
+            ->connection
+            ->executeQuery(
+                'SELECT * FROM `' . $this->table . '`',
+                ['schema' => $schema->getName()]
+            );
+
+        if (empty($stmt->fetchAll())) {
+            $this->addSql('INSERT INTO `' . $this->table . '`
+             (`id`, `name`, `sequence`, `created_at`, `updated_at`, `code`)
+                VALUES 
+                (1,\'label.not_started\',0,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.not_started\'),
+                (2,\'label.in_progress\',1,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.in_progress\'),
+                (3,\'label.pending\',2,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.pending\'),
+                (4,\'label.open\',3,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.open\'),
+                (5,\'label.closed\',4,\'2017-08-31 02:41:27\',\'2019-06-25 12:34:45\',\'label.closed\')
+            ');
+        }
     }
 
     /**
@@ -38,6 +47,6 @@ class Version20200811110857 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        $this->addSql('DELETE FROM `' . $this->table. '`');
+        $this->addSql('DELETE FROM `' . $this->table . '`');
     }
 }
