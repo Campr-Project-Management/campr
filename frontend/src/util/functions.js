b62f014696e3acaae50a44c04adf8fa85a2b0cc5
alex84 <alexx1984@ukr.net>
2020-09-02 15:48:06 +0300

    documment ready removed

--- frontend/src/util/functions.js
+++ frontend/src/util/functions.js
@@ -23,26 +23,23 @@ export function replaceBadInputs(val) {
  * @return {void} alert
  */
 export function timepicerMask() {
-    $(document).ready(function() {
-        $('.mx-input').keyup(function() {
-            let val = this.value;
-            let lastLength;
-            do {
-                // Loop over the input to apply rules repeately to pasted inputs
-                lastLength = val.length;
-                val = replaceBadInputs(val);
-            } while(val.length > 0 && lastLength !== val.length);
-            this.value = val;
-            if(this.value.length == 2) {
-                this.value += ':';
-            };
-        });
-
-        // Check the final result when the input has lost focus
-        $('.mx-input').blur(function() {
-            let val = this.value;
-            val = (/^(([01][0-9]|2[0-3])h)|(([01][0-9]|2[0-3]):[0-5][0-9])$/.test(val) ? val : '');
-            this.value = val;
-        });
+    $('.mx-input').keyup(function() {
+        let val = this.value;
+        let lastLength;
+        do {
+            // Loop over the input to apply rules repeately to pasted inputs
+            lastLength = val.length;
+            val = replaceBadInputs(val);
+        } while (val.length > 0 && lastLength !== val.length);
+        this.value = val;
+        if (this.value.length == 2) {
+            this.value += ':';
+        };
+    });
+    // Check the final result when the input has lost focus
+    $('.mx-input').blur(function() {
+        let val = this.value;
+        val = (/^(([01][0-9]|2[0-3])h)|(([01][0-9]|2[0-3]):[0-5][0-9])$/.test(val) ? val : '');
+        this.value = val;
     });
 }
